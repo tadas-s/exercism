d255d81f36a165e23d565191b25ffbeef225f38b
Tadas Sasnauskas <tadas@yoyo.lt>
2016-06-21 22:50:40 +0100

    Rust: even more improved bob!

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -2,15 +2,11 @@ pub fn reply(phrase: &str) -> &str {
     match phrase {
         "" => "Fine. Be that way!",
         _ if is_shouting(phrase) => "Whoa, chill out!",
-        _ if is_question(phrase) => "Sure.",
+        _ if phrase.ends_with("?") => "Sure.",
         _ => "Whatever."
     }
 }
 
 fn is_shouting(phrase: &str) -> bool {
     !phrase.is_empty() && phrase == phrase.to_uppercase()
-}
-
-fn is_question(phrase: &str) -> bool {
-    phrase.chars().last().unwrap_or('-') == '?'
 }
