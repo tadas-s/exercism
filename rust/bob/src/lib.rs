16149b22016576a2e4f952fd960b8a10991781ea
Tadas Sasnauskas <tadas@yoyo.lt>
2016-06-27 21:31:03 +0100

    Rust: bit simpler bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,12 +1,9 @@
 pub fn reply(phrase: &str) -> &str {
     match phrase {
         "" => "Fine. Be that way!",
-        _ if is_shouting(phrase) => "Whoa, chill out!",
+        _ if phrase == phrase.to_uppercase() => "Whoa, chill out!",
         _ if phrase.ends_with("?") => "Sure.",
         _ => "Whatever."
     }
 }
 
-fn is_shouting(phrase: &str) -> bool {
-    !phrase.is_empty() && phrase == phrase.to_uppercase()
-}
