5d0d6055aa1d44425c461b9bd93b3518a6522fe2
Tadas Sasnauskas <tadas@yoyo.lt>
2016-09-06 14:14:22 +0100

    Remove flawed pluralisation

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -14,18 +14,12 @@ fn line(line: u8) -> String {
     match line {
         0 => "No more bottles of beer on the wall, no more bottles of beer.".to_string(),
         1 => "Go to the store and buy some more, 99 bottles of beer on the wall.".to_string(),
+        2 => "1 bottle of beer on the wall, 1 bottle of beer.".to_string(),
         3 => "Take it down and pass it around, no more bottles of beer on the wall.".to_string(),
-        _ if line % 2 == 0 => format!("{} of beer on the wall, {} of beer.", pluralize_bottle_pile_of(line / 2), pluralize_bottle_pile_of(line / 2)),
-        _ if line % 2 == 1 => format!("Take one down and pass it around, {} of beer on the wall.", pluralize_bottle_pile_of(line / 2 - 1)),
+        5 => "Take one down and pass it around, 1 bottle of beer on the wall.".to_string(),
+        _ if line % 2 == 0 => format!("{} bottles of beer on the wall, {} bottles of beer.", line / 2, line / 2),
+        _ if line % 2 == 1 => format!("Take one down and pass it around, {} bottles of beer on the wall.", line / 2 - 1),
         _ => panic!("This will never happen. This also looks like not a great code as well.")
-    }
-}
-
-fn pluralize_bottle_pile_of(number: u8) -> String {
-    match number {
-        0 => "no more bottles".to_string(),
-        _ if (number % 10 == 1) && (number != 11) => format!("{} bottle", number),
-        _ => format!("{} bottles", number)
     }
 }
 
