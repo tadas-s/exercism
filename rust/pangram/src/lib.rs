aad24105d21d3e1597300e7221e59708cf0ebb00
Tadas Sasnauskas <tadas@yoyo.lt>
2016-09-07 13:19:05 +0100

    Reworked pangram solution

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,11 +1,6 @@
+static ALPHABET: &'static str = "abcdefghijklmnopqrstuvwxyz";
+
 pub fn is_pangram(s: &str) -> bool {
     let lowercased = s.to_lowercase();
-
-    for c in ('a' as u8)..('z' as u8 + 1) {
-        if !lowercased.contains(c as char) {
-            return false;
-        }
-    }
-
-    return true;
+    ALPHABET.chars().all(|c| lowercased.contains(c))
 }
