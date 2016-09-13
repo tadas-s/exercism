c7c2648ea0e6161dbdd43a537bdad689bb3f88a6
Tadas Sasnauskas <tadas@yoyo.lt>
2016-09-13 16:23:58 +0300

    Misc cleanup

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -6,11 +6,13 @@ use regex::Regex;
 pub fn word_count(string: &str) -> HashMap<String, u32> {
     let re = Regex::new(r"[^A-Za-z0-9]+").unwrap();
     let mut counts: HashMap<String, u32> = HashMap::new();
+    
     for word in re.split(&string) {
-        if word.len() > 0 {
+        if !word.is_empty() {
             let counter = counts.entry(word.to_lowercase().to_string()).or_insert(0);
             *counter += 1;
         }
     }
+    
     return counts;
 }
