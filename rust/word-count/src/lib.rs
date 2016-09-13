e6395f2d751b62760b40d0f33a120efc0e79b9b0
Tadas Sasnauskas <tadas@yoyo.lt>
2016-09-13 16:17:29 +0300

    Solution: word count

---
+++ rust/word-count/src/lib.rs
@@ -0,0 +1,16 @@
+extern crate regex;
+
+use std::collections::HashMap;
+use regex::Regex;
+
+pub fn word_count(string: &str) -> HashMap<String, u32> {
+    let re = Regex::new(r"[^A-Za-z0-9]+").unwrap();
+    let mut counts: HashMap<String, u32> = HashMap::new();
+    for word in re.split(&string) {
+        if word.len() > 0 {
+            let counter = counts.entry(word.to_lowercase().to_string()).or_insert(0);
+            *counter += 1;
+        }
+    }
+    return counts;
+}
