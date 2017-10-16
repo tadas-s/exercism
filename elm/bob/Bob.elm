2032345a04bd49aa211a4efe392d2f60f8f173d4
Tadas Sasnauskas <tadas.sasnauskas@carwow.co.uk>
2017-10-16 09:38:37 +0100

    bob, wip

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,1 +1,21 @@
 module Bob exposing (..)
+
+import String exposing ( toUpper, toLower, endsWith )
+
+type Statement = Stating String | Asking String | Shouting String
+
+discern : String -> Statement
+discern statement =
+  if endsWith "?" statement then
+    Asking statement
+  else if (statement == toUpper statement) && (statement /= toLower statement) then
+    Shouting statement
+  else
+    Stating statement
+
+hey : String -> String
+hey statement =
+  case discern statement of
+    Asking statement -> "Sure."
+    Shouting statement -> "Whoa, chill out!"
+    Stating statement -> "Whatever."
--- elm/bob/tests/Tests.elm
+++ elm/bob/tests/Tests.elm
@@ -15,62 +15,51 @@ tests =
             \() ->
                 Expect.equal "Whatever."
                     (Bob.hey "Tom-ay-to, tom-aaaah-to.")
-        , skip <|
-            test "shouting" <|
-                \() ->
-                    Expect.equal
-                        "Whoa, chill out!"
-                        (Bob.hey "WATCH OUT!")
-        , skip <|
-            test "shouting gibberish" <|
+        , test "shouting" <|
+            \() ->
+                Expect.equal "Whoa, chill out!"
+                    (Bob.hey "WATCH OUT!")
+        , test "shouting gibberish" <|
                 \() ->
                     Expect.equal
                         "Whoa, chill out!"
                         (Bob.hey (uppercaseGibberish 10))
-        , skip <|
-            test "asking a question" <|
+        , test "asking a question" <|
                 \() ->
                     Expect.equal
                         "Sure."
                         (Bob.hey "Does this cryogenic chamber make me look fat?")
-        , skip <|
-            test "asking a numeric question" <|
+        , test "asking a numeric question" <|
                 \() ->
                     Expect.equal
                         "Sure."
                         (Bob.hey "You are, what, like 15?")
-        , skip <|
-            test "asking gibberish" <|
+        , test "asking gibberish" <|
                 \() ->
                     Expect.equal
                         "Sure."
                         (Bob.hey (gibberishQuestion 20))
-        , skip <|
-            test "talking forcefully" <|
+        , test "talking forcefully" <|
                 \() ->
                     Expect.equal
                         "Whatever."
                         (Bob.hey "Let's go make out behind the gym!")
-        , skip <|
-            test "using acronyms in regular speech" <|
+        , test "using acronyms in regular speech" <|
                 \() ->
                     Expect.equal
                         "Whatever."
                         (Bob.hey "It's OK if you don't want to go to the DMV.")
-        , skip <|
-            test "forceful questions" <|
+        , test "forceful questions" <|
                 \() ->
                     Expect.equal
                         "Whoa, chill out!"
                         (Bob.hey "WHAT THE HELL WERE YOU THINKING?")
-        , skip <|
-            test "shouting numbers" <|
+        , test "shouting numbers" <|
                 \() ->
                     Expect.equal
                         "Whoa, chill out!"
                         (Bob.hey "1, 2, 3 GO!")
-        , skip <|
-            test "only numbers" <|
+        , test "only numbers" <|
                 \() ->
                     Expect.equal
                         "Whatever."
