540e5de0488ee2440f6e8e308d4ce4aee3b60eb8
Tadas Sasnauskas <tadas@yoyo.lt>
2018-07-23 20:07:45 +0100

    r track: misc tweak the difference of squares

--- r/difference-of-squares/difference-of-squares.R
+++ r/difference-of-squares/difference-of-squares.R
@@ -2,6 +2,6 @@
 # and should return the difference-of-squares as described
 # in the README.md
 difference_of_squares <- function(natural_number) {
-    sequence = 0:natural_number
+    sequence <- 0:natural_number
     sum(sequence) ^ 2 - sum(sequence ^ 2)
 }
