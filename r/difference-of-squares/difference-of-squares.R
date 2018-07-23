07d286e77a1ea28a19750cf1e5073eeccf96dd82
Tadas Sasnauskas <tadas@yoyo.lt>
2018-07-23 07:48:47 +0100

    r track: difference of squares fix
    There are some less verbose functions to do sum + vectors (collections? arrays?)
    can be squared directly.

--- r/difference-of-squares/difference-of-squares.R
+++ r/difference-of-squares/difference-of-squares.R
@@ -2,8 +2,6 @@
 # and should return the difference-of-squares as described
 # in the README.md
 difference_of_squares <- function(natural_number) {
-    square_of_sum = Reduce('+', 1:natural_number, 0) ^ 2
-    sum_of_squares = Reduce('+', Map(function(x) x^2, 1:natural_number), 0)
-
-    square_of_sum - sum_of_squares
+    sequence = 0:natural_number
+    sum(sequence) ^ 2 - sum(sequence ^ 2)
 }
