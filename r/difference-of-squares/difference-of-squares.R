074c6357c4a13c8570ec99b47b274c0ec93fce69
Tadas Sasnauskas <tadas@yoyo.lt>
2018-07-23 07:37:40 +0100

    r track: difference of squares solution

--- r/difference-of-squares/difference-of-squares.R
+++ r/difference-of-squares/difference-of-squares.R
@@ -2,4 +2,8 @@
 # and should return the difference-of-squares as described
 # in the README.md
 difference_of_squares <- function(natural_number) {
+    square_of_sum = Reduce('+', 1:natural_number, 0) ^ 2
+    sum_of_squares = Reduce('+', Map(function(x) x^2, 1:natural_number), 0)
+
+    square_of_sum - sum_of_squares
 }
