10ce0b58eacfcaccf40edf4312d085a3ca18dca1
Tadas Sasnauskas <tadas@yoyo.lt>
2018-07-24 08:19:04 +0100

    r track: perfect numbers solution

--- r/perfect-numbers/perfect-numbers.R
+++ r/perfect-numbers/perfect-numbers.R
@@ -1,3 +1,17 @@
-number_type <- function(n){
-  
+number_type <- function(n) {
+    if (n <= 0) {
+        stop(sprintf("n must be > 0, but %d was given", n))
+    }
+
+    aliquot_sum = sum(
+        Filter(function(x) { n %% x == 0 }, 1:(n/2))
+    )
+
+    if (n == 1 || aliquot_sum < n) {
+        return("deficient")
+    } else if (aliquot_sum == n) {
+        return("perfect")
+    } else if (aliquot_sum > n) {
+        return("abundant")
+    }
 }
