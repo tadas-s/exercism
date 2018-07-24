c236352d00d4760a1213b19b824a630c9a3334f6
Tadas Sasnauskas <tadas@yoyo.lt>
2018-07-24 21:23:05 +0100

    r track: hammind distance updated solution

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -1,24 +1,9 @@
 # This is a stub function to take two strings
 # and calculate the hamming distance
 hamming <- function(strand1, strand2) {
-    strand1_characters = unlist(strsplit(strand1, ""))
-    strand2_characters = unlist(strsplit(strand2, ""))
-
-    if (length(strand1_characters) != length(strand2_characters)) {
+    if (nchar(strand1) != nchar(strand2)) {
         stop("Lengths of both strands must match")
     }
 
-    if (length(strand1_characters) == 0) {
-        return(0)
-    }
-
-    distance = 0
-
-    for(index in 1:length(strand1_characters)) {
-        if(strand1_characters[index] != strand2_characters[index]) {
-            distance = distance + 1
-        }
-    }
-
-    distance
+    sum(strsplit(strand1, "")[[1]] != strsplit(strand2, "")[[1]])
 }
