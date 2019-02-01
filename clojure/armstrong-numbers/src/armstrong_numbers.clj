597235598f2a2a88f8a1533e4313a423d4514419
Tadas Sasnauskas <tadas@yoyo.lt>
2019-02-01 22:04:34 +0000

    clojure: Armstrong Numbers. Tests pass, but still doesn't feel right ;)

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -2,8 +2,10 @@
 
 (defn digits [number] (map #(long (Character/digit % 10)) (str number)))
 
+;; Integers only base^exponent operation
+(defn pow [base exponent]
+  (reduce * (repeat exponent base)))
+
 (defn armstrong? [num]
-  (let [all_digits (digits num)]
-      ( = num (long (reduce + (map #(Math/pow % (count all_digits)) all_digits))))
-    )
-  )
+  (let [all_digits (digits num) number_of_digits (count all_digits)]
+    (= num (reduce + (map #(pow % number_of_digits) all_digits)))))
