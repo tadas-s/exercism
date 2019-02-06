5118e3d0f3cb1ce1b475c7200c51d9709f23a1fe
Tadas Sasnauskas <tadas@yoyo.lt>
2019-02-06 08:33:11 +0000

    clojure: two-fer solution

--- clojure/two-fer/src/two_fer.clj
+++ clojure/two-fer/src/two_fer.clj
@@ -1,5 +1,5 @@
 (ns two-fer)
 
-(defn two-fer [name] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn two-fer
+  ([] "One for you, one for me.")
+  ([name] (format "One for %s, one for me." name)))
