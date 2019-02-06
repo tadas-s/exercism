4b08a5aff3fd4f30e79629e7c36d3c8344546a80
Tadas Sasnauskas <tadas@yoyo.lt>
2019-02-06 07:56:42 +0000

    clojure: Armstrong Numbers - rewrite some code using ->> (thrush) operator
    ¯\_(ツ)_/¯

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,20 +1,20 @@
 (ns armstrong-numbers)
 
 (defn- pow [base exponent]
-  (reduce * 
+  (reduce *
           (repeat exponent base)))
 
 (defn- digits [num]
   (+ 1 (int (Math/floor (Math/log10 num)))))
 
 (defn- dividers [num]
-  (take-while #(> num (mod num %)) 
+  (take-while #(> num (rem num %))
               (iterate #(* 10 %) 1)))
 
 (defn armstrong? [num]
   (let [total_digits (digits num)]
-    (= num
-       (reduce +
-               (map #(pow % total_digits)
-                    (map #(quot (mod num (* 10 %)) %)
-                         (dividers num)))))))
+    (->> (dividers num)
+         (map #(quot (rem num (* 10 %)) %))
+         (map #(pow % total_digits))
+         (reduce +)
+         (= num))))
