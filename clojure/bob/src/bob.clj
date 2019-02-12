d5a1a401f6053fd0d1fdb12ca56cbd8d1907b7d6
Tadas Sasnauskas <tadas@yoyo.lt>
2019-02-12 07:43:56 +0000

    clojure: bob solution

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,5 +1,21 @@
 (ns bob)
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(use '[clojure.string :only (upper-case trim ends-with?)])
+
+(defn- silence? [s]
+  (= (trim s) ""))
+
+(defn- asking? [s]
+  (ends-with? s "?"))
+
+(defn- shouting? [s]
+  (and (= s (upper-case s))
+       (some? (re-find (re-matcher #"[a-zA-Z]+" s)))))
+
+(defn response-for [s]
+  (cond
+    (silence? s) "Fine. Be that way!"
+    (and (asking? s) (shouting? s)) "Calm down, I know what I'm doing!"
+    (asking? s) "Sure."
+    (shouting? s) "Whoa, chill out!"
+    :else "Whatever."))
