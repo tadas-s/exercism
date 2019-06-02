9cea56fe46f13c0c1d900877a51970f14c9124fb
Tadas Sasnauskas <tadas@yoyo.lt>
2019-06-02 18:05:34 +0100

    rust: raindrops solution

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let pling = n % 3 == 0;
+    let plang = n % 5 == 0;
+    let plong = n % 7 == 0;
+
+    if pling || plang || plong {
+        let mut rainspeak = String::from("");
+        if pling { rainspeak.push_str("Pling") }
+        if plang { rainspeak.push_str("Plang") }
+        if plong { rainspeak.push_str("Plong") }
+        return rainspeak;
+    } else {
+        return n.to_string();
+    }
 }
