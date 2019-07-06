7e30fd1ddbc996e6488f6e09417be7577ed9625a
Tadas Sasnauskas <tadas@yoyo.lt>
2019-07-06 13:59:11 +0100

    rust: nth-prime - little denser code using ranges / iterators

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,5 +1,5 @@
 struct Primes {
-    current: Option<u32>
+    current: Option<u32>,
 }
 
 impl Primes {
@@ -11,21 +11,14 @@ impl Primes {
      * Trial division prime test: https://en.wikipedia.org/wiki/Trial_division
      */
     fn is_prime(n: u32) -> bool {
-        if n <= 1 {
-            return false;
-        } else if n <= 3 {
-            return true;
-        } else if n % 2 == 0 || n % 3 == 0 {
-            return false;
-        } else {
-            let mut divisor = 5;
-            while divisor * divisor <= n {
-                if n % divisor == 0 || n % (divisor + 2) == 0 {
-                    return false;
-                }
-                divisor += 6
-            }
-            return true;
+        match n {
+            1 => false,
+            2 | 3 => true,
+            _ if n % 2 == 0 || n % 3 == 0 => false,
+            _ => !(5..)
+                .step_by(6)
+                .take_while(|divisor| divisor * divisor <= n)
+                .any(|divisor| n % divisor == 0 || n % (divisor + 2) == 0),
         }
     }
 }
@@ -36,25 +29,17 @@ impl Iterator for Primes {
     fn next(&mut self) -> Option<u32> {
         match self.current {
             Some(prime) => {
-                let mut next_prime = prime;
-
-                loop {
-                    next_prime = next_prime + 1;
-
-                    if Primes::is_prime(next_prime) {
-                        break;
-                    }
-                }
-                self.current = Some(next_prime);
+                self.current = ((prime + 1)..).find(|&number| Primes::is_prime(number));
             }
-            _ => { self.current = Some(2) }
+            _ => self.current = Some(2),
         }
 
-        return self.current;
+        self.current
     }
 }
 
 pub fn nth(n: u32) -> u32 {
     let mut primes: Primes = Primes::new();
-    return primes.nth(n as usize).unwrap();
+
+    primes.nth(n as usize).unwrap()
 }
