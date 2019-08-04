12feb1093776a0699dd69e792bc55be08f072673
Tadas Sasnauskas <tadas@yoyo.lt>
2019-08-04 13:47:12 +0100

    rust: proverb solution

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    if list.len() > 0 {
+        let mut proverb: String = list
+            .windows(2)
+            .map(|pieces| format!("For want of a {} the {} was lost.\n", pieces[0], pieces[1]))
+            .collect::<Vec<_>>()
+            .join("");
+
+        proverb.push_str(&format!("And all for the want of a {}.", list[0]));
+
+        proverb
+    } else {
+        String::from("")
+    }
 }
