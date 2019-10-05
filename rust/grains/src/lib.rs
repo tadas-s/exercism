4d808ca5aa75abf226f79a2c9cbe18e725177d46
Tadas Sasnauskas <tadas@yoyo.lt>
2019-10-05 15:01:42 +0100

    rust: grains - use bit shift instead of pow
    Unnecessary attempt at optimizing for speed.

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn square(s: u32) -> u64 {
-    if (s < 1 || s > 64) {
+    if s < 1 || s > 64 {
         panic!("Square must be between 1 and 64");
     }
 
-    2u64.pow(s - 1)
+    1 << (s-1)
 }
 
 pub fn total() -> u64 {
