dcb80734af327aaabe0d7f5f4267dba356388b49
Tadas Sasnauskas <tadas@yoyo.lt>
2019-10-05 14:04:35 +0100

    rust: sum-of-multiples solution
    First "I have no idea what I'm doing" version. But tests pass ¯\_(ツ)_/¯

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,7 +1,12 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    let mut multiples = factors
+        .iter()
+        .filter(|&f| *f > 0)
+        .flat_map(|f| (1..=((limit - 1) / f)).map(move |multiplier| f * multiplier))
+        .collect::<Vec<u32>>();
+
+    multiples.sort();
+    multiples.dedup();
+
+    multiples.iter().sum()
 }
