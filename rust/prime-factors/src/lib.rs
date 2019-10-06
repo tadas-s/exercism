85c35b645dbb574c89ba6e4a4b9d48a6dcf6cd4b
Tadas Sasnauskas <tadas@yoyo.lt>
2019-10-06 13:32:30 +0100

    rust: prime-factors solution

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut result = Vec::new();
+    let mut remaining = n;
+    let mut candidate = 2;
+
+    while remaining > 1 {
+        while remaining % candidate == 0 {
+            result.push(candidate);
+            remaining /= candidate;
+        }
+
+        candidate += 1;
+    }
+
+    result
 }
