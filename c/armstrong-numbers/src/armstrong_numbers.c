82a6c9816bf3119a5324ca3aba1d7d0631172192
Tadas Sasnauskas <tadas@yoyo.lt>
2021-05-15 14:02:23 +0100

    c: armstrong-numbers solution

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,1 +1,14 @@
 #include "armstrong_numbers.h"
+#include <math.h>
+
+bool is_armstrong_number(int candidate) {
+    int no_of_digits = floor(log10f(candidate)) + 1;
+    int sum_of_pows = 0;
+
+    for (int i = 0; i < no_of_digits; i++) {
+        int digit = (candidate % (int)pow(10, i + 1)) / pow(10, i);
+        sum_of_pows += pow(digit, no_of_digits);
+    }
+
+    return sum_of_pows == candidate;
+}
