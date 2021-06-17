750e01f40868b439f88b0fb438ac150d4d19fa9d
Tadas Sasnauskas <tadas@yoyo.lt>
2021-06-17 16:59:47 +0100

    c: isogram solution

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,1 +1,21 @@
 #include "isogram.h"
+
+bool is_isogram(const char phrase[]) {
+    if (phrase == NULL) { return false; }
+
+    size_t length = strlen(phrase);
+
+    if (length == 0) { return true; }
+
+    for (size_t i = 0; i < length - 1; i++) {
+        if (isalpha(phrase[i])) {
+            for(size_t j = i + 1; j < length; j++) {
+                if (tolower(phrase[i]) == tolower(phrase[j])) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return true;
+}
