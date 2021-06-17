6f021e10525b01d4e44690d20b56d71c3b574b1c
Tadas Sasnauskas <tadas@yoyo.lt>
2021-06-17 11:34:17 +0100

    c: resistor-color-duo solution

--- c/resistor-color-duo/src/resistor_color_duo.c
+++ c/resistor-color-duo/src/resistor_color_duo.c
@@ -1,1 +1,5 @@
 #include "resistor_color_duo.h"
+
+int color_code(resistor_band_t colors[]) {
+    return 10 * colors[0] + colors[1];
+}
--- c/resistor-color-duo/src/resistor_color_duo.h
+++ c/resistor-color-duo/src/resistor_color_duo.h
@@ -1,4 +1,21 @@
 #ifndef RESISTOR_COLOR_DUO_H
 #define RESISTOR_COLOR_DUO_H
 
+#include <stdint.h>
+
+typedef enum {
+    BLACK = 0,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE
+} resistor_band_t;
+
+int color_code(resistor_band_t colors[]);
+
 #endif
