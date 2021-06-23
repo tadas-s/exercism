511ea25de2044488738a34ba2470b415bcfc8fc4
Tadas Sasnauskas <tadas@yoyo.lt>
2021-06-23 09:49:01 +0100

    c: rna-transcription solution

--- c/rna-transcription/src/rna_transcription.c
+++ c/rna-transcription/src/rna_transcription.c
@@ -1,1 +1,41 @@
 #include "rna_transcription.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *to_rna(const char *dna) {
+    if (dna == NULL) {
+        return NULL;
+    }
+
+    char *rna = malloc(strlen(dna) + 1);
+    size_t offset = 0;
+
+    while (1) {
+        switch (dna[offset]) {
+            case '\0':
+                rna[offset] = '\0';
+                return rna;
+                break;
+            case 'G':
+                rna[offset] = 'C';
+                break;
+            case 'C':
+                rna[offset] = 'G';
+                break;
+            case 'T':
+                rna[offset] = 'A';
+                break;
+            case 'A':
+                rna[offset] = 'U';
+                break;
+            default:
+                free(rna);
+                return NULL;
+                break;
+        }
+
+        offset++;
+    }
+
+    return rna;
+}
