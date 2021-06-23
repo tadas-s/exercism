712e07b85d9fc5326064730c378b5c9d52e791bd
Tadas Sasnauskas <tadas@yoyo.lt>
2021-06-23 09:24:00 +0100

    c: hamming solution

--- c/hamming/src/hamming.c
+++ c/hamming/src/hamming.c
@@ -1,1 +1,27 @@
 #include "hamming.h"
+#include <stddef.h>
+
+int compute(const char *lhs, const char *rhs) {
+    if (lhs == NULL || rhs == NULL) {
+        return -1;
+    }
+
+    size_t offset = 0;
+    int distance = 0;
+
+    while (1) {
+        if (lhs[offset] == '\0' && rhs[offset] == '\0') {
+            return distance;
+        } else if (lhs[offset] == '\0' || rhs[offset] == '\0') {
+            return -1;
+        } else {
+            if (lhs[offset] != rhs[offset]) {
+                distance++;
+            }
+        }
+
+        offset++;
+    }
+
+    return distance;
+}
