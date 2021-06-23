80154a96091e727bae5609c4064204f063be0320
Tadas Sasnauskas <tadas@yoyo.lt>
2021-06-23 17:28:30 +0100

    c: grains solution

--- c/grains/src/grains.c
+++ c/grains/src/grains.c
@@ -1,1 +1,19 @@
 #include "grains.h"
+
+uint64_t square(uint8_t index) {
+    if (index > 0 && index < 65) {
+        return 1ull << (index - 1);
+    } else {
+        return 0;
+    }
+}
+
+uint64_t total(void) {
+    uint64_t total = 0;
+
+    for(int i = 1; i < 65; i++) {
+        total += square(i);
+    }
+
+    return total;
+}
