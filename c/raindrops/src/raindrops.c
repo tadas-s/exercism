daeb63acc717525ffcc5639a312b6918adf7eed8
Tadas Sasnauskas <tadas@yoyo.lt>
2021-06-27 11:58:40 +0100

    c: raindrops solution

--- c/raindrops/src/raindrops.c
+++ c/raindrops/src/raindrops.c
@@ -1,1 +1,25 @@
 #include "raindrops.h"
+#include <string.h>
+#include <stdio.h>
+
+char *convert(char result[], int drops) {
+    result[0] = '\0';
+
+    if (drops % 3 == 0) {
+        strcat(result, "Pling");
+    }
+
+    if (drops % 5 == 0) {
+        strcat(result, "Plang");
+    }
+
+    if (drops % 7 == 0) {
+        strcat(result, "Plong");
+    }
+
+    if (!(drops % 3 == 0 || drops % 5 == 0 || drops % 7 == 0)) {
+        sprintf(result, "%d", drops);
+    }
+
+    return result;
+}
