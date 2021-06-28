db6af73720ab82a8b6fe656333c070d5e8c7e9f7
Tadas Sasnauskas <tadas@yoyo.lt>
2021-06-28 09:18:39 +0100

    c: sum-of-multiples solution

--- c/sum-of-multiples/src/sum_of_multiples.c
+++ c/sum-of-multiples/src/sum_of_multiples.c
@@ -1,1 +1,16 @@
 #include "sum_of_multiples.h"
+
+unsigned int sum(const unsigned int *factors, const size_t number_of_factors, const unsigned int limit) {
+    unsigned int total = 0;
+
+    for (unsigned int n = 1; n < limit; n++) {
+        for (size_t factor = 0; factor < number_of_factors; factor++) {
+            if (factors[factor] > 0 && n % factors[factor] == 0) {
+                total += n;
+                break;
+            }
+        }
+    }
+
+    return total;
+}
