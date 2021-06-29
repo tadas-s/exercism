3521ba9ab1e7cea9c7114c88faa8c3aa4b9b2c33
Tadas Sasnauskas <tadas@yoyo.lt>
2021-06-29 10:44:30 +0100

    c: square-root solution

--- c/square-root/src/square_root.c
+++ c/square-root/src/square_root.c
@@ -1,1 +1,23 @@
 #include "square_root.h"
+
+int32_t square_root(int32_t num) {
+    int32_t res = 0;
+    int32_t bit = 1 << 30;
+
+    while (bit > num) {
+        bit >>= 2;
+    }
+
+    while (bit != 0) {
+        if (num >= res + bit) {
+            num -= res + bit;
+            res = (res >> 1) + bit;
+        } else {
+            res >>= 1;
+        }
+
+        bit >>= 2;
+    }
+
+    return res;
+}
--- c/square-root/src/square_root.h
+++ c/square-root/src/square_root.h
@@ -1,4 +1,8 @@
 #ifndef SQUARE_ROOT_H
 #define SQUARE_ROOT_H
 
+#include <stdint.h>
+
+int32_t square_root(int32_t num);
+
 #endif
