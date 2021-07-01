77b9bbe781041eef10dc1ed1ac05f75b072d3848
Tadas Sasnauskas <tadas@yoyo.lt>
2021-07-01 11:12:29 +0100

    c: binary-search solution

--- c/binary-search/src/binary_search.c
+++ c/binary-search/src/binary_search.c
@@ -1,1 +1,20 @@
 #include "binary_search.h"
+
+const int *binary_search(int value, const int *arr, size_t length) {
+    int lower_bound = 0;
+    int upper_bound = length - 1;
+
+    while (lower_bound <= upper_bound) {
+        int i = (lower_bound + upper_bound) / 2;
+
+        if (arr[i] < value) {
+            lower_bound = i + 1;
+        } else if (arr[i] > value) {
+            upper_bound = i - 1;
+        } else {
+            return &arr[i];
+        }
+    }
+
+    return NULL;
+}
