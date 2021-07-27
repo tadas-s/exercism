6ca72003b3ab6355999fe75a25ac3cb978ac1c82
Tadas Sasnauskas <tadas@yoyo.lt>
2021-07-27 14:15:26 +0100

    c: triangle solution

--- c/triangle/src/triangle.c
+++ c/triangle/src/triangle.c
@@ -1,1 +1,22 @@
 #include "triangle.h"
+
+bool is_valid(triangle_t t);
+
+bool is_equilateral(triangle_t t) {
+    return is_valid(t) && t.a == t.b && t.b == t.c;
+}
+
+bool is_isosceles(triangle_t t) {
+    return is_valid(t) && (t.a == t.b || t.b == t.c || t.c == t.a);
+}
+
+bool is_scalene(triangle_t t) {
+    return is_valid(t) && (t.a != t.b && t.b != t.c);
+}
+
+bool is_valid(triangle_t t) {
+    return t.a > 0 && t.b > 0 && t.c > 0 &&
+           (t.a + t.b > t.c) &&
+           (t.a + t.c > t.b) &&
+           (t.b + t.c > t.a);
+}
