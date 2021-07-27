2328a8332b316645d0372ee06ac2bd15c21c1789
Tadas Sasnauskas <tadas@yoyo.lt>
2021-07-27 14:52:34 +0100

    c: perfect-numbers solution

--- c/perfect-numbers/src/perfect_numbers.c
+++ c/perfect-numbers/src/perfect_numbers.c
@@ -1,1 +1,23 @@
 #include "perfect_numbers.h"
+
+kind classify_number(int32_t number) {
+    int32_t sum = 0;
+
+    if (number < 1) {
+        return ERROR;
+    }
+
+    for (int32_t d = 1; d <= (number / 2); d++) {
+        if (number % d == 0) {
+            sum += d;
+        }
+    }
+
+    if (sum == number) {
+        return PERFECT_NUMBER;
+    } else if (sum > number) {
+        return ABUNDANT_NUMBER;
+    } else {
+        return DEFICIENT_NUMBER;
+    }
+}
--- c/perfect-numbers/src/perfect_numbers.h
+++ c/perfect-numbers/src/perfect_numbers.h
@@ -1,11 +1,15 @@
 #ifndef PERFECT_NUMBERS_H
 #define PERFECT_NUMBERS_H
 
+#include <stdint.h>
+
 typedef enum {
    PERFECT_NUMBER = 1,
    ABUNDANT_NUMBER = 2,
    DEFICIENT_NUMBER = 3,
    ERROR = -1
 } kind;
+
+kind classify_number(int32_t number);
 
 #endif
