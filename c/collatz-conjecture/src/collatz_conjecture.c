5c3a41a934ff21e6c228f0293537fb9771519729
Tadas Sasnauskas <tadas@yoyo.lt>
2021-07-28 10:12:34 +0100

    c: collatz-conjecture solution

--- c/collatz-conjecture/src/collatz_conjecture.c
+++ c/collatz-conjecture/src/collatz_conjecture.c
@@ -1,1 +1,21 @@
 #include "collatz_conjecture.h"
+
+int steps(int n) {
+    int s = 0;
+
+    if (n < 1) {
+        return ERROR_VALUE;
+    }
+
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+
+        s++;
+    };
+
+    return s;
+}
