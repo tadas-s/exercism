4233abca1dcc50d2ee4787c2e336d9c7aafd549d
Tadas Sasnauskas <tadas@yoyo.lt>
2021-07-28 10:25:58 +0100

    c: binary solution

--- c/binary/src/binary.c
+++ c/binary/src/binary.c
@@ -1,1 +1,19 @@
 #include "binary.h"
+#include <string.h>
+
+int convert(const char *input) {
+    const size_t length = strlen(input);
+    int decimal = 0;
+
+    for (size_t i = 0; i < length; i++) {
+        switch(input[i]) {
+            case '0': break;
+            case '1':
+                decimal |= 1 << (length - (i + 1));
+            break;
+            default: return INVALID; break;
+        }
+    }
+
+    return decimal;
+}
