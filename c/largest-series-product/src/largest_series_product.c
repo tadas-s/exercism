7fb854f26e2cd8d13e0084308af19b0fcb83a44b
Tadas Sasnauskas <tadas@yoyo.lt>
2021-07-28 20:22:27 +0100

    c: largest-series-product solution

--- c/largest-series-product/src/largest_series_product.c
+++ c/largest-series-product/src/largest_series_product.c
@@ -1,1 +1,25 @@
 #include "largest_series_product.h"
+#include <string.h>
+
+int64_t largest_series_product(char *digits, size_t span) {
+    const size_t length = strlen(digits);
+    int64_t max = -1;
+
+    for (size_t i = 0; i < (length - span + 1); i++) {
+        int64_t product = 1;
+
+        for (size_t j = 0; j < span; j++) {
+            int8_t digit = digits[i+j] - '0';
+
+            if (digit < 0 || digit > 9) { return -1; }
+
+            product = product * digit;
+        }
+
+        if (product > max) {
+            max = product;
+        }
+    }
+
+    return max;
+}
