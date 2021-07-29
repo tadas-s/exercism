ebca33a0087fd88b0a56cfabc6ad46673c476fe4
Tadas Sasnauskas <tadas@yoyo.lt>
2021-07-29 10:25:18 +0100

    c: scrabble solution

--- c/scrabble-score/src/scrabble_score.c
+++ c/scrabble-score/src/scrabble_score.c
@@ -1,1 +1,25 @@
 #include "scrabble_score.h"
+#include <stdint.h>
+#include <stddef.h>
+#include <ctype.h>
+
+static uint8_t points[255] = {
+    ['a'] = 1, ['e'] = 1, ['i'] = 1, ['o'] = 1,
+    ['u'] = 1, ['l'] = 1, ['n'] = 1, ['r'] = 1,
+    ['s'] = 1, ['t'] = 1, ['d'] = 2, ['g'] = 2,
+    ['b'] = 3, ['c'] = 3, ['m'] = 3, ['p'] = 3,
+    ['f'] = 4, ['h'] = 4, ['v'] = 4, ['w'] = 4,
+    ['y'] = 4, ['k'] = 5, ['j'] = 8, ['x'] = 8,
+    ['q'] = 10, ['z']= 10
+};
+
+unsigned int score(const char *word) {
+    int result = 0;
+
+    for (size_t i = 0; word[i] != '\0'; i++) {
+        if (points[tolower(word[i])] == 0) { return 0; }
+        result += points[tolower(word[i])];
+    }
+
+    return result;
+}
--- c/scrabble-score/test/test_scrabble_score.c
+++ c/scrabble-score/test/test_scrabble_score.c
@@ -16,62 +16,57 @@ static void test_lowercase_letter(void)
 
 static void test_uppercase_letter(void)
 {
-   TEST_IGNORE();               // delete this line to run test
    TEST_ASSERT_EQUAL(1, score("A"));
 }
 
 static void test_valuable_letter(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(4, score("f"));
 }
 
 static void test_short_word(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(2, score("at"));
 }
 
 static void test_short_valuable_word(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(12, score("zoo"));
 }
 
 static void test_medium_word(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(6, score("street"));
 }
 
 static void test_medium_valuable_word(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(22, score("quirky"));
 }
 
 static void test_long_mixed_case_word(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(41, score("OxyphenButazone"));
 }
 
 static void test_english_like_word(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(8, score("pinata"));
 }
 
 static void test_empty_input(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(0, score(""));
 }
 
 static void test_entire_alphabet_available(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(87, score("abcdefghijklmnopqrstuvwxyz"));
+}
+
+static void test_non_letters_retur_error(void)
+{
+   TEST_ASSERT_EQUAL(0, score("this?"));
 }
 
 int main(void)
@@ -89,6 +84,7 @@ int main(void)
    RUN_TEST(test_english_like_word);
    RUN_TEST(test_empty_input);
    RUN_TEST(test_entire_alphabet_available);
+   RUN_TEST(test_non_letters_retur_error);
 
    return UnityEnd();
 }
