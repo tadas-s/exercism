aeb7febf65c966dd86894ad5cad4fcf77ddc265b
Tadas Sasnauskas <tadas@yoyo.lt>
2021-07-30 15:51:18 +0100

    c: matching-brackets solution using stack

--- c/matching-brackets/src/matching_brackets.c
+++ c/matching-brackets/src/matching_brackets.c
@@ -1,69 +1,34 @@
 #include "matching_brackets.h"
-#include <stdbool.h>
 #include <stdlib.h>
-
-typedef struct {
-    size_t length;
-    bool error;
-} bracket_length_t;
+#include <string.h>
 
 bool is_openning_bracket(const char c);
 bool is_closing_bracket(const char c);
 char get_closing_bracket(const char c);
-bracket_length_t bracketed_string_lenght(const char *input);
 
 bool is_paired(const char *input) {
-    size_t offset = 0;
+    char *stack = malloc(strlen(input));
+    size_t stack_top = 0;
 
-    while (input[offset] != '\0') {
-        const char c = input[offset];
+    for (size_t i = 0; input[i] != '\0'; i++) {
+        const char c = input[i];
 
         if (is_openning_bracket(c)) {
-            bracket_length_t bl = bracketed_string_lenght(&input[offset]);
-
-            if (bl.error) {
+            stack[stack_top] = c;
+            stack_top++;
+        } else if (is_closing_bracket(c)) {
+            if (stack_top > 0 && get_closing_bracket(stack[stack_top - 1]) == c) {
+                stack_top--;
+            } else {
+                free(stack);
                 return false;
-            } else {
-                offset += bl.length + 2;
             }
-        } else if (is_closing_bracket(c)) {
-            return false;
-        } else {
-            offset++;
         }
     }
 
-    return true;
-}
+    free(stack);
 
-bracket_length_t bracketed_string_lenght(const char *input) {
-    bracket_length_t result = { 0, false };
-    size_t offset = 1;
-    const char cb = get_closing_bracket(input[0]);
-
-    while (input[offset] != '\0') {
-        const char c = input[offset];
-
-        if (is_openning_bracket(c)) {
-            bracket_length_t bl = bracketed_string_lenght(&input[offset]);
-
-            if (bl.error) {
-                return (bracket_length_t){ 0, true };
-            } else {
-                offset += bl.length + 2;
-                result.length += bl.length + 2;
-            }
-        } else if (c == cb) {
-            return result;
-        } else if (is_closing_bracket(c)) {
-            return (bracket_length_t){ 0, true };
-        } else {
-            offset++;
-            result.length++;
-        }
-    }
-
-    return (bracket_length_t){ 0, true };
+    return stack_top == 0;
 }
 
 bool is_openning_bracket(const char c) {
