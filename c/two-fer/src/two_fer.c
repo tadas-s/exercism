442f9e945201eb0d3498794a59bf884b20133d37
Tadas Sasnauskas <tadas@yoyo.lt>
2021-08-03 16:59:08 +0100

    c: two-fer solution

--- c/two-fer/src/two_fer.c
+++ c/two-fer/src/two_fer.c
@@ -1,1 +1,7 @@
 #include "two_fer.h"
+#include <stdio.h>
+
+void two_fer(char *buffer, const char *name) {
+    const char *you = name ? name : "you";
+    sprintf(buffer, "One for %s, one for me.", you);
+}
