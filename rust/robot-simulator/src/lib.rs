2331ddd48a0c2995a8871f87528a503b28331a2b
Tadas Sasnauskas <tadas@yoyo.lt>
2023-01-12 10:24:03 +0200

    rust: robot-simulator solution
    Bit unclear why test suite sort of forces you to use immutable structs and
    ton of copying.

--- rust/robot-simulator/src/lib.rs
+++ rust/robot-simulator/src/lib.rs
@@ -1,6 +1,9 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
 
+use core::panic;
+use Direction::*;
+
 #[derive(PartialEq, Eq, Debug)]
 pub enum Direction {
     North,
@@ -9,41 +12,64 @@ pub enum Direction {
     West,
 }
 
-pub struct Robot;
+pub struct Robot {
+    x: i32,
+    y: i32,
+    direction: Direction,
+}
 
 impl Robot {
     pub fn new(x: i32, y: i32, d: Direction) -> Self {
-        unimplemented!("Create a robot at (x, y) ({}, {}) facing {:?}", x, y, d,)
+        Self { x: x, y: y, direction: d}
     }
 
     #[must_use]
     pub fn turn_right(self) -> Self {
-        unimplemented!()
+        match self.direction {
+            North => Self { direction: East, ..self },
+            East => Self { direction: South, ..self },
+            South => Self { direction: West, ..self },
+            West => Self { direction: North, ..self },
+        }
     }
 
     #[must_use]
     pub fn turn_left(self) -> Self {
-        unimplemented!()
+        match self.direction {
+            North => Self { direction: West, ..self },
+            East => Self { direction: North, ..self },
+            South => Self { direction: East, ..self },
+            West => Self { direction: South, ..self },
+        }
     }
 
     #[must_use]
     pub fn advance(self) -> Self {
-        unimplemented!()
+        match self.direction {
+            North => Self { y: self.y + 1, ..self },
+            East => Self { x: self.x + 1, ..self },
+            South => Self { y: self.y - 1, ..self },
+            West => Self { x: self.x - 1, ..self },
+        }
     }
 
     #[must_use]
     pub fn instructions(self, instructions: &str) -> Self {
-        unimplemented!(
-            "Follow the given sequence of instructions: {}",
-            instructions
-        )
+        instructions.chars().fold(self, |robot, instruction| {
+            match instruction {
+                'R' => robot.turn_right(),
+                'L' => robot.turn_left(),
+                'A' => robot.advance(),
+                _ => panic!("Unknown instruction: '{}'", instruction),
+            }
+        })
     }
 
     pub fn position(&self) -> (i32, i32) {
-        unimplemented!()
+        (self.x, self.y)
     }
 
     pub fn direction(&self) -> &Direction {
-        unimplemented!()
+        &self.direction
     }
 }
