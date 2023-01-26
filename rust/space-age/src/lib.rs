d3be28b2aff4d42f19ddc2736700fe2c5ac410e5
Tadas Sasnauskas <tadas@yoyo.lt>
2023-01-26 17:51:24 +0200

    rust: space-age solution

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,21 +1,19 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    seconds: u64
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        Self { seconds: s }
     }
 }
 
 pub trait Planet {
+    const EARTH_YEAR_MULTIPLIER: f64;
+
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
+        (d.seconds as f64) / (Self::EARTH_YEAR_MULTIPLIER * (31557600 as f64))
     }
 }
 
@@ -28,11 +26,34 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    const EARTH_YEAR_MULTIPLIER: f64 = 0.2408467;
+}
+
+impl Planet for Venus {
+    const EARTH_YEAR_MULTIPLIER: f64 = 0.61519726;
+}
+
+impl Planet for Earth {
+    const EARTH_YEAR_MULTIPLIER: f64 = 1.0;
+}
+
+impl Planet for Mars {
+    const EARTH_YEAR_MULTIPLIER: f64 = 1.8808158;
+}
+
+impl Planet for Jupiter {
+    const EARTH_YEAR_MULTIPLIER: f64 = 11.862615;
+}
+
+impl Planet for Saturn {
+    const EARTH_YEAR_MULTIPLIER: f64 = 29.447498;
+}
+
+impl Planet for Uranus {
+    const EARTH_YEAR_MULTIPLIER: f64 = 84.016846;
+}
+
+impl Planet for Neptune {
+    const EARTH_YEAR_MULTIPLIER: f64 = 164.79132;
+}
