d004315ee0069de5bafaa71c7df7f2906068dcdb
Tadas Sasnauskas <tadas@yoyo.lt>
2023-01-26 17:57:09 +0200

    rust: space-age solution variant 2 - try using macros

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -10,50 +10,27 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    const EARTH_YEAR_MULTIPLIER: f64;
+    const ORBIT_MULTIPLIER: f64;
 
     fn years_during(d: &Duration) -> f64 {
-        (d.seconds as f64) / (Self::EARTH_YEAR_MULTIPLIER * (31557600 as f64))
+        (d.seconds as f64) / (Self::ORBIT_MULTIPLIER * (31557600 as f64))
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
-impl Planet for Mercury {
-    const EARTH_YEAR_MULTIPLIER: f64 = 0.2408467;
+macro_rules! planet {
+    ($struct_name:ident, $orbit_multiplier:expr) => {
+        pub struct $struct_name;
+        impl Planet for $struct_name {
+            const ORBIT_MULTIPLIER: f64 = $orbit_multiplier;
+        }
+    };
 }
 
-impl Planet for Venus {
-    const EARTH_YEAR_MULTIPLIER: f64 = 0.61519726;
-}
-
-impl Planet for Earth {
-    const EARTH_YEAR_MULTIPLIER: f64 = 1.0;
-}
-
-impl Planet for Mars {
-    const EARTH_YEAR_MULTIPLIER: f64 = 1.8808158;
-}
-
-impl Planet for Jupiter {
-    const EARTH_YEAR_MULTIPLIER: f64 = 11.862615;
-}
-
-impl Planet for Saturn {
-    const EARTH_YEAR_MULTIPLIER: f64 = 29.447498;
-}
-
-impl Planet for Uranus {
-    const EARTH_YEAR_MULTIPLIER: f64 = 84.016846;
-}
-
-impl Planet for Neptune {
-    const EARTH_YEAR_MULTIPLIER: f64 = 164.79132;
-}
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
