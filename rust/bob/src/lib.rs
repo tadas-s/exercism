60b7ccd86b1277cc9159ae17c33990a09d104702
Tadas Sasnauskas <tadas@yoyo.lt>
2023-01-28 14:34:13 +0200

    rust: bob solution

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,13 @@
-pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+pub fn reply(phrase: &str) -> &str {
+    let shouting = phrase.contains(char::is_alphabetic) && phrase == phrase.to_uppercase();
+    let trimmed = phrase.trim();
+    let questioning = trimmed.ends_with("?");
+
+    match trimmed {
+        "" => "Fine. Be that way!",
+        _ if questioning && shouting => "Calm down, I know what I'm doing!",
+        _ if shouting => "Whoa, chill out!",
+        _ if questioning => "Sure.",
+        _ => "Whatever.",
+    }
 }
