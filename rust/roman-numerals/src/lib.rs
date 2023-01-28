3c3923d3d41a6bf98e19b32eef42842dbce540ef
Tadas Sasnauskas <tadas@yoyo.lt>
2023-01-28 15:35:27 +0200

    rust: roman-numerals solution

--- rust/roman-numerals/src/lib.rs
+++ rust/roman-numerals/src/lib.rs
@@ -1,15 +1,35 @@
-use std::fmt::{Display, Formatter, Result};
+use std::fmt::{Display, Error, Formatter, Result};
 
-pub struct Roman;
+const UNITS: &'static [[&'static str; 10]; 4] = &[
+    ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+    ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+    ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+    ["", "M", "MM", "MMM", "", "", "", "", "", ""],
+];
+
+pub struct Roman {
+    number: u32,
+}
 
 impl Display for Roman {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> Result {
-        unimplemented!("Return a roman-numeral string representation of the Roman object");
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        if self.number == 0 || self.number > 3999 {
+            return Err(Error);
+        }
+
+        write!(
+            f,
+            "{}{}{}{}",
+            UNITS[3][(self.number / 1000) as usize],
+            UNITS[2][(self.number % 1000 / 100) as usize],
+            UNITS[1][(self.number % 100 / 10) as usize],
+            UNITS[0][(self.number % 10) as usize],
+        )
     }
 }
 
 impl From<u32> for Roman {
     fn from(num: u32) -> Self {
-        unimplemented!("Construct a Roman object from the '{}' number", num);
+        Roman { number: num }
     }
 }
