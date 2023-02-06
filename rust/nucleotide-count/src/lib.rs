df3607aea2d27673d0e794170d3cfe7adf65cc3b
Tadas Sasnauskas <tadas@yoyo.lt>
2023-02-06 09:38:03 +0200

    rust: nucleotide-count solution

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,16 +1,28 @@
 use std::collections::HashMap;
 
+static NUCLEOTIDES: [char; 4] = ['C', 'T', 'A', 'G'];
+
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    unimplemented!(
-        "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
-        nucleotide,
-        dna
-    );
+    if !NUCLEOTIDES.contains(&nucleotide) {
+        return Err(nucleotide);
+    }
+
+    if let Some(bad_nucleotide_in_sequence) = dna.chars().find(|n| !NUCLEOTIDES.contains(n)) {
+        return Err(bad_nucleotide_in_sequence);
+    }
+
+    Ok(dna.chars().filter(|&n| nucleotide == n).count())
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    unimplemented!(
-        "How much of every nucleotide type is contained inside DNA string '{}'?",
-        dna
-    );
+    let mut counts = HashMap::with_capacity(NUCLEOTIDES.len());
+
+    for nucleotide in NUCLEOTIDES {
+        match count(nucleotide, dna) {
+            Ok(count) => counts.insert(nucleotide, count),
+            Err(nucleotide) => return Err(nucleotide),
+        };
+    }
+
+    Ok(counts)
 }
