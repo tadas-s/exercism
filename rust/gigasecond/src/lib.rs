20fcff25a5098237e1871b05ed3088aed612bf89
Tadas Sasnauskas <tadas@yoyo.lt>
2025-12-09 13:38:47 +0200

    Start the Rust track from scratch
    I probably forgot most of stuff anyway ¯\_(ツ)_/¯

--- rust/gigasecond/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use time::ext::NumericalDuration;
-use time::PrimitiveDateTime as DateTime;
-
-// Returns a DateTime one billion seconds after start.
-pub fn after(start: DateTime) -> DateTime {
-    start + 1_000_000_000.seconds()
-}
