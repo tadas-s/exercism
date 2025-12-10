87e75e94a33ab09c229e9cf94f8ef28539c2194e
Tadas Sasnauskas <tadas@yoyo.lt>
2025-12-10 21:55:16 +0200

    rust: sublist solution

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,7 +7,18 @@ pub enum Comparison {
 }
 
 pub fn sublist(first_list: &[i32], second_list: &[i32]) -> Comparison {
-    todo!(
-        "Determine if the {first_list:?} is equal to, sublist of, superlist of or unequal to {second_list:?}."
-    );
+    let first_list_sublist_of_second_list =
+        first_list.is_empty() ||
+        second_list.windows(first_list.len()).any(|w| w == first_list);
+
+    let second_list_sublist_of_first_list =
+        second_list.is_empty() ||
+        first_list.windows(second_list.len()).any(|w| w == second_list);
+
+    match (first_list_sublist_of_second_list, second_list_sublist_of_first_list) {
+        (true, true) => Comparison::Equal,
+        (true, false) => Comparison::Sublist,
+        (false, true) => Comparison::Superlist,
+        (false, false) => Comparison::Unequal
+    }
 }
