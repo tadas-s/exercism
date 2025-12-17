e240a5a418e3fb695d170c177cf72f5a5c3bb398
Tadas Sasnauskas <tadas@yoyo.lt>
2025-12-17 15:02:26 +0200

    rust: high-scores solution

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,24 +1,31 @@
 #[derive(Debug)]
-pub struct HighScores;
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
 
-impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        todo!("Construct a HighScores struct, given the scores: {scores:?}")
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        Self { scores }
     }
 
     pub fn scores(&self) -> &[u32] {
-        todo!("Return all the scores as a slice")
+        self.scores
     }
 
     pub fn latest(&self) -> Option<u32> {
-        todo!("Return the latest (last) score")
+        self.scores.last().cloned()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        todo!("Return the highest score")
+        self.scores.iter().max().cloned()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        todo!("Return 3 highest scores")
+        let mut top = self.scores.to_vec();
+        top.sort();
+        top.reverse();
+        top.truncate(3);
+
+        top
     }
 }
