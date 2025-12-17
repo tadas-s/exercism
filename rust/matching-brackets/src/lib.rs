bf205fa59070be73cc0e6ebc52b6b53bd98ddde6
Tadas Sasnauskas <tadas@yoyo.lt>
2025-12-17 15:12:00 +0200

    rust: matching-brackets solution

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,3 +1,26 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    todo!("Check if the string \"{string}\" contains balanced brackets");
+    let mut stack: Vec<char> = vec![];
+
+    for c in string.chars() {
+        match c {
+            '[' | '{' | '(' => stack.push(c),
+            ']' | '}' | ')' => {
+                if stack.pop() != Some(opening_bracket(c)) {
+                    return false;
+                }
+            }
+            _ => (),
+        }
+    }
+
+    stack.is_empty()
+}
+
+fn opening_bracket(c: char) -> char {
+    match c {
+        ']' => '[',
+        '}' => '{',
+        ')' => '(',
+        _ => panic!("No opening bracket for char '{c}'"),
+    }
 }
