ede71af21bec6d9c31b3ae278d04a8a103d70911
Tadas Sasnauskas <tadas@yoyo.lt>
2025-12-17 17:22:22 +0200

    rust: series solution

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,3 +1,7 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    todo!("What are the series of length {len} in string {digits:?}")
+    digits.chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|w| w.iter().collect())
+        .collect()
 }
