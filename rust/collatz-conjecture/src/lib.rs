d0874094eeb4a6933e565267188985868f5df955
Tadas Sasnauskas <tadas@yoyo.lt>
2025-12-17 15:38:29 +0200

    rust: collatz-conjecture solution

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,3 +1,18 @@
-pub fn collatz(n: u64) -> Option<u64> {
-    todo!("return Some(x) where x is the number of steps required to reach 1 starting with {n}")
+pub fn collatz(mut n: u64) -> Option<u64> {
+    if n < 1 {
+        return None;
+    }
+
+    let mut steps = 0;
+
+    while n > 1 {
+        n = match n {
+            n if n % 2 == 0 => n / 2,
+            n => n * 3 + 1,
+        };
+
+        steps += 1;
+    }
+
+    Some(steps)
 }
