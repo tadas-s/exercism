313f39753fa74076d401b5f2e6c89a763d3afeeb
Tadas Sasnauskas <tadas@yoyo.lt>
2025-12-22 14:39:42 +0200

    rust: eliuds-eggs solution

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,3 +1,10 @@
-pub fn egg_count(display_value: u32) -> usize {
-    todo!("count the eggs in {display_value}")
+pub fn egg_count(mut display_value: u32) -> usize {
+    let mut count = 0;
+
+    while display_value > 0 {
+        if display_value % 2 == 1 { count += 1 }
+        display_value /= 2;
+    }
+
+    count
 }
