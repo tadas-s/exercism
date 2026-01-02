f50b7755cac7016d3fc0995343e765088c1372a9
Tadas Sasnauskas <tadas@yoyo.lt>
2026-01-02 15:25:11 +0200

    rust: all-your-base solution

--- rust/all-your-base/src/lib.rs
+++ rust/all-your-base/src/lib.rs
@@ -1,8 +1,11 @@
+use num::{checked_pow, pow, CheckedMul, Num, ToPrimitive};
+use std::fmt::Debug;
+
 #[derive(Debug, PartialEq, Eq)]
-pub enum Error {
+pub enum Error<T> {
     InvalidInputBase,
     InvalidOutputBase,
-    InvalidDigit(u32),
+    InvalidDigit(T),
 }
 
 ///
@@ -35,6 +38,52 @@ pub enum Error {
 ///  * Never output leading 0 digits, unless the input number is 0, in which the output must be `[0]`.
 ///    However, your function must be able to process input with leading 0 digits.
 ///
-pub fn convert(number: &[u32], from_base: u32, to_base: u32) -> Result<Vec<u32>, Error> {
-    todo!("Convert {number:?} from base {from_base} to base {to_base}")
+pub fn convert<T: PartialOrd + Copy + Num + CheckedMul + ToPrimitive>(
+    number: &[T],
+    from_base: T,
+    to_base: T,
+) -> Result<Vec<T>, Error<T>> {
+    let mut n = T::zero();
+
+    if from_base <= T::one() {
+        return Err(Error::InvalidInputBase);
+    }
+    if to_base <= T::one() {
+        return Err(Error::InvalidOutputBase);
+    }
+
+    for (exponent, &digit) in number.iter().rev().enumerate() {
+        if digit >= from_base {
+            return Err(Error::InvalidDigit(digit));
+        }
+
+        if digit > T::zero() {
+            // to mult overflow if input has leading zero and is close to format maximum
+            n = n + pow(from_base, exponent) * digit;
+        }
+    }
+
+    if n == T::zero() {
+        return Ok(vec![T::zero()]);
+    }
+
+    let mut result: Vec<T> = vec![];
+
+    for exponent in 0.. {
+        if n % pow(to_base, exponent) == n {
+            break;
+        }
+
+        if let Some(next_divisor) = checked_pow(to_base, exponent + 1) {
+            result.push(n % next_divisor / pow(to_base, exponent));
+        } else {
+            // This is the last most significant digit
+            result.push(n / pow(to_base, exponent));
+            break;
+        }
+    }
+
+    result.reverse();
+
+    Ok(result)
 }
