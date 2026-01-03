f6df8631acfc102ffe36d8c77985e8ead096895f
Tadas Sasnauskas <tadas@yoyo.lt>
2026-01-03 17:54:26 +0200

    rust: binary-search solution

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,5 +1,21 @@
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    todo!(
-        "Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index."
-    );
+use std::cmp::Ordering::{Equal, Greater, Less};
+
+pub fn find<T: AsRef<[U]>, U: Ord>(array: T, key: U) -> Option<usize> {
+    let array = array.as_ref();
+
+    let mut midpoint: usize;
+    let mut left: usize = 0;
+    let mut right: usize = array.len();
+
+    while left < right {
+        midpoint = (left + right) / 2;
+
+        match array[midpoint].cmp(&key) {
+            Equal => return Some(midpoint),
+            Less => left = midpoint + 1,
+            Greater => right = midpoint,
+        };
+    }
+
+    None
 }
