b3cfdb4326a5a38962877731c41fcf41d5e7aff9
Tadas Sasnauskas <tadas@yoyo.lt>
2026-01-07 19:11:10 +0200

    rust: robot-simulator solution

--- rust/robot-simulator/src/lib.rs
+++ rust/robot-simulator/src/lib.rs
@@ -1,5 +1,5 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
+use Direction::*;
+use core::panic;
 
 #[derive(PartialEq, Eq, Debug)]
 pub enum Direction {
@@ -9,38 +9,100 @@ pub enum Direction {
     West,
 }
 
-pub struct Robot;
+pub struct Robot {
+    x: i32,
+    y: i32,
+    direction: Direction,
+}
 
 impl Robot {
-    pub fn new(x: i32, y: i32, d: Direction) -> Self {
-        todo!("Create a robot at (x, y) ({x}, {y}) facing {d:?}")
+    pub fn new(x: i32, y: i32, direction: Direction) -> Self {
+        Self { x, y, direction }
     }
 
     #[must_use]
     pub fn turn_right(self) -> Self {
-        todo!()
+        match self.direction {
+            North => Self {
+                direction: East,
+                ..self
+            },
+            East => Self {
+                direction: South,
+                ..self
+            },
+            South => Self {
+                direction: West,
+                ..self
+            },
+            West => Self {
+                direction: North,
+                ..self
+            },
+        }
     }
 
     #[must_use]
     pub fn turn_left(self) -> Self {
-        todo!()
+        match self.direction {
+            North => Self {
+                direction: West,
+                ..self
+            },
+            East => Self {
+                direction: North,
+                ..self
+            },
+            South => Self {
+                direction: East,
+                ..self
+            },
+            West => Self {
+                direction: South,
+                ..self
+            },
+        }
     }
 
     #[must_use]
     pub fn advance(self) -> Self {
-        todo!()
+        match self.direction {
+            North => Self {
+                y: self.y + 1,
+                ..self
+            },
+            East => Self {
+                x: self.x + 1,
+                ..self
+            },
+            South => Self {
+                y: self.y - 1,
+                ..self
+            },
+            West => Self {
+                x: self.x - 1,
+                ..self
+            },
+        }
     }
 
     #[must_use]
     pub fn instructions(self, instructions: &str) -> Self {
-        todo!("Follow the given sequence of instructions: {instructions}")
+        instructions
+            .chars()
+            .fold(self, |robot, instruction| match instruction {
+                'R' => robot.turn_right(),
+                'L' => robot.turn_left(),
+                'A' => robot.advance(),
+                _ => panic!("Unknown instruction: '{}'", instruction),
+            })
     }
 
     pub fn position(&self) -> (i32, i32) {
-        todo!()
+        (self.x, self.y)
     }
 
     pub fn direction(&self) -> &Direction {
-        todo!()
+        &self.direction
     }
 }
