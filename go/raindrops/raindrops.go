7e79fb9d03d32bee15375b01f8b6a6282543473f
Tadas Sasnauskas <tadas@yoyo.lt>
2026-07-29 16:53:33 +0300

    go: raindrops solution

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,25 @@
 package raindrops
 
-func Convert(number int) string {
-	panic("Please implement the Convert function")
+import (
+  "strconv"
+)
+
+func Convert(number int) (result string) {
+	if number%3 == 0 {
+		result += "Pling"
+	}
+
+	if number%5 == 0 {
+		result += "Plang"
+	}
+
+	if number%7 == 0 {
+		result += "Plong"
+	}
+
+	if result == "" {
+		result = strconv.Itoa(number)
+	}
+
+	return
 }
