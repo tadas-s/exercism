f82f0ec006b149786d46715b7df0042a3934201f
Tadas Sasnauskas <tadas@yoyo.lt>
2026-07-29 17:54:42 +0300

    go: hamming solution

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,19 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return -1, errors.New("inputs are not the same length")
+	}
+
+	distance := 0
+
+	for i := range a {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
