725601da6073250861502d776786863d0df0d745
Tadas Sasnauskas <tadas@yoyo.lt>
2026-07-30 16:57:58 +0300

    go: darts solution

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,5 +1,18 @@
 package darts
 
+import "math"
+
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+	distance := math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2))
+
+	switch {
+	case distance <= 1.0:
+		return 10
+	case distance <= 5.0:
+		return 5
+	case distance <= 10.0:
+		return 1
+	default:
+		return 0
+	}
 }
