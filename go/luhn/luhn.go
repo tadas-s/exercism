42ebb01891cf2d6e705c063492f4d88815f9b285
Tadas Sasnauskas <tadas@yoyo.lt>
2026-07-30 17:43:16 +0300

    go: luhn solution

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,5 +1,30 @@
 package luhn
 
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	var sum int64 = 0
+	var digits uint = 0
+
+	for i := len(id) - 1; i >= 0; i-- {
+		if id[i] >= '0' && id[i] <= '9' {
+			var digit = id[i] - '0'
+
+			if digits%2 > 0 {
+				if digit*2 > 9 {
+					sum += int64(digit*2 - 9)
+				} else {
+					sum += int64(digit * 2)
+				}
+			} else {
+				sum += int64(digit)
+			}
+
+			digits++
+		} else if id[i] == ' ' {
+			continue // skip spaces
+		} else {
+			return false
+		}
+	}
+
+	return digits > 1 && sum%10 == 0
 }
