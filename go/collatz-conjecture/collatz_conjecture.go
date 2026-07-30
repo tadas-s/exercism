34ccd8a15b6b84d68cff2bb01389525a6b0506af
Tadas Sasnauskas <tadas@yoyo.lt>
2026-07-30 16:10:10 +0300

    go: collatz-conjecture solution

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,21 @@
 package collatzconjecture
 
+import "fmt"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n < 1 {
+		return 0, fmt.Errorf("given number %d must be greater than zero", n)
+	}
+
+	steps := 0
+
+	for ; n > 1; steps++ {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = n*3 + 1
+		}
+	}
+
+	return steps, nil
 }
