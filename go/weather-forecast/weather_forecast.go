bc6907207a60e4626b59bf2ea3a3b07e1aa635c3
Tadas Sasnauskas <tadas@yoyo.lt>
2026-07-30 15:44:46 +0300

    go: weather-forecast solution

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,10 +1,14 @@
+// Package weather provides utilities for formatting weather forecasts.
 package weather
 
 var (
+	// CurrentCondition keeps track of most recently used weather condition.
 	CurrentCondition string
-	CurrentLocation  string
+	// CurrentLocation keeps track of most recently used location.
+	CurrentLocation string
 )
 
+// Forecast formats a weather forecast string.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
