65d6c08e610c0d1cacf1486608e6eee4b8238857
Tadas Sasnauskas <tadas@yoyo.lt>
2026-08-05 10:25:58 +0300

    go: isbn-verifier solution

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,23 @@
 package isbnverifier
 
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	var digitsCollected = 0
+	var sum = 0
+	var lastPos = len(isbn) - 1
+
+	for i, char := range isbn {
+		if i <= lastPos && char >= '0' && char <= '9' {
+			sum += int(char-'0') * (10 - digitsCollected)
+			digitsCollected++
+		} else if i == lastPos && char == 'X' {
+			sum += 10
+			digitsCollected++
+		} else if char == '-' && (digitsCollected == 1 || digitsCollected == 4 || digitsCollected == 9) {
+			continue
+		} else {
+			return false
+		}
+	}
+
+	return digitsCollected == 10 && sum%11 == 0
 }
