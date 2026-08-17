817bdfd9e580eb3f563c67d679c7f60528e05692
Tadas Sasnauskas <tadas@yoyo.lt>
2026-08-17 14:33:47 +0300

    go: sum-of-multiples solution

--- go/sum-of-multiples/sum_of_multiples.go
+++ go/sum-of-multiples/sum_of_multiples.go
@@ -1,5 +1,16 @@
 package sumofmultiples
 
 func SumMultiples(limit int, divisors ...int) int {
-	panic("Please implement the SumMultiples function")
+	sum := 0
+
+	for n := 1; n < limit; n++ {
+		for _, divisor := range divisors {
+			if divisor > 0 && n%divisor == 0 {
+				sum += n
+				break
+			}
+		}
+	}
+
+	return sum
 }
