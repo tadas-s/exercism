206788bda688190c91df6b975fe7168f0deb04f6
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-02 16:11:39 +0300

    go: anagram exercise

---
+++ go/anagram/anagram_test.go
@@ -0,0 +1,27 @@
+package anagram
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestDetectAnagrams(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			actual := Detect(tc.subject, tc.candidates)
+			slices.Sort(actual)
+			slices.Sort(tc.expected)
+			if !slices.Equal(tc.expected, actual) {
+				t.Errorf("Detect(%q, %#v) = %#v, want: %#v", tc.subject, tc.candidates, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkDetectAnagrams(b *testing.B) {
+	for b.Loop() {
+		for _, tc := range testCases {
+			Detect(tc.subject, tc.candidates)
+		}
+	}
+}
