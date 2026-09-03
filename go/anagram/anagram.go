4dbd6fe0c39d0340334840bcee8f7c0dae1d4016
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-03 09:44:34 +0300

    go: anagram solution

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,5 +1,30 @@
 package anagram
 
+import (
+	"slices"
+	"strings"
+)
+
 func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+	var result []string
+
+	sortedSubject := sortedRunes(subject)
+
+	for _, candidate := range candidates {
+		if strings.ToLower(subject) != strings.ToLower(candidate) && sortedSubject == sortedRunes(candidate) {
+			result = append(result, candidate)
+		}
+	}
+
+	return result
+}
+
+func sortedRunes(s string) string {
+	runes := []rune(strings.ToLower(s))
+
+	slices.SortFunc(runes, func(a, b rune) int {
+		return strings.Compare(string(a), string(b))
+	})
+
+	return string(runes)
 }
