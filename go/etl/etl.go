d70fae7db21708fc0a0e1632c298efb0e6e88112
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-08 11:45:24 +0300

    go: etl solution

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,5 +1,15 @@
 package etl
 
+import "strings"
+
 func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+	out := make(map[string]int)
+
+	for points, letters := range in {
+		for _, letter := range letters {
+			out[strings.ToLower(letter)] = points
+		}
+	}
+
+	return out
 }
