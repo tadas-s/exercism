920e00822f10c23664bab0fcc334a0e009f232f7
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-08 16:15:35 +0300

    go: armstrong-numbers solution

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,17 @@
 package armstrongnumbers
 
+import (
+	"math"
+)
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	numOfDigits := int(math.Floor(math.Log10(float64(n)))) + 1
+	sumOfPows := 0
+
+	for i := range numOfDigits {
+		digit := (n % int(math.Pow(10, float64(i+1)))) / int(math.Pow(10, float64(i)))
+		sumOfPows += int(math.Pow(float64(digit), float64(numOfDigits)))
+	}
+
+	return sumOfPows == n
 }
