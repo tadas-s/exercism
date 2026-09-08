41c08ef63ac43614cad7a30980e57f1eb965b13b
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-08 11:47:21 +0300

    go: list-ops exercise

---
+++ go/list-ops/cases_test.go
@@ -0,0 +1,219 @@
+// This file contains tests from the shared problem specifications repo.
+package listops
+
+// This is an auto-generated file. Do not change it manually. Run the generator to update the file.
+// See https://github.com/exercism/go#synchronizing-tests-and-instructions
+// Source: exercism/problem-specifications
+// Commit: 25c2ae5 build(deps): bump fast-uri from 3.1.0 to 3.1.2 (#2653)
+
+type testCaseAppend struct {
+	description  string
+	list1, list2 IntList
+	expected     IntList
+}
+
+var testCasesAppend = []testCaseAppend{
+	{
+		description: "empty lists",
+		list1:       IntList{},
+		list2:       IntList{},
+		expected:    IntList{},
+	},
+	{
+		description: "list to empty list",
+		list1:       IntList{},
+		list2:       IntList{1, 2, 3, 4},
+		expected:    IntList{1, 2, 3, 4},
+	},
+	{
+		description: "empty list to list",
+		list1:       IntList{1, 2, 3, 4},
+		list2:       IntList{},
+		expected:    IntList{1, 2, 3, 4},
+	},
+	{
+		description: "non-empty lists",
+		list1:       IntList{1, 2},
+		list2:       IntList{2, 3, 4, 5},
+		expected:    IntList{1, 2, 2, 3, 4, 5},
+	},
+}
+
+type testCaseConcat struct {
+	description string
+	lists       []IntList
+	expected    IntList
+}
+
+var testCasesConcat = []testCaseConcat{
+	{
+		description: "empty list",
+		lists:       []IntList{},
+		expected:    IntList{},
+	},
+	{
+		description: "list of lists",
+		lists:       []IntList{{1, 2}, {3}, {}, {4, 5, 6}},
+		expected:    IntList{1, 2, 3, 4, 5, 6},
+	},
+}
+
+type testCaseFilter struct {
+	description string
+	list        IntList
+	function    func(int) bool
+	functionStr string
+	expected    IntList
+}
+
+var testCasesFilter = []testCaseFilter{
+	{
+		description: "empty list",
+		list:        IntList{},
+		function:    func(x int) bool { return x%2 == 1 },
+		functionStr: "(x) -> x modulo 2 == 1",
+		expected:    IntList{},
+	},
+	{
+		description: "non-empty list",
+		list:        IntList{1, 2, 3, 5},
+		function:    func(x int) bool { return x%2 == 1 },
+		functionStr: "(x) -> x modulo 2 == 1",
+		expected:    IntList{1, 3, 5},
+	},
+}
+
+type testCaseFoldl struct {
+	description string
+	list        IntList
+	initial     int
+	function    func(int, int) int
+	functionStr string
+	expected    int
+}
+
+var testCasesFoldl = []testCaseFoldl{
+	{
+		description: "direction dependent function applied to non-empty list",
+		list:        IntList{2, 5},
+		initial:     5,
+		function:    func(x, y int) int { return x / y },
+		functionStr: "(x, y) -> x / y",
+		expected:    0,
+	},
+	{
+		description: "empty list",
+		list:        IntList{},
+		initial:     2,
+		function:    func(acc, el int) int { return el * acc },
+		functionStr: "(acc, el) -> el * acc",
+		expected:    2,
+	},
+	{
+		description: "direction independent function applied to non-empty list",
+		list:        IntList{1, 2, 3, 4},
+		initial:     5,
+		function:    func(acc, el int) int { return el + acc },
+		functionStr: "(acc, el) -> el + acc",
+		expected:    15,
+	},
+}
+
+type testCaseFoldr struct {
+	description string
+	list        IntList
+	initial     int
+	function    func(int, int) int
+	functionStr string
+	expected    int
+}
+
+var testCasesFoldr = []testCaseFoldr{
+	{
+		description: "direction dependent function applied to non-empty list",
+		list:        IntList{2, 5},
+		initial:     5,
+		function:    func(x, y int) int { return x / y },
+		functionStr: "(x, y) -> x / y",
+		expected:    2,
+	},
+	{
+		description: "empty list",
+		list:        IntList{},
+		initial:     2,
+		function:    func(acc, el int) int { return el * acc },
+		functionStr: "(acc, el) -> el * acc",
+		expected:    2,
+	},
+	{
+		description: "direction independent function applied to non-empty list",
+		list:        IntList{1, 2, 3, 4},
+		initial:     5,
+		function:    func(acc, el int) int { return el + acc },
+		functionStr: "(acc, el) -> el + acc",
+		expected:    15,
+	},
+}
+
+type testCaseLength struct {
+	description string
+	list        IntList
+	expected    int
+}
+
+var testCasesLength = []testCaseLength{
+	{
+		description: "empty list",
+		list:        IntList{},
+		expected:    0,
+	},
+	{
+		description: "non-empty list",
+		list:        IntList{1, 2, 3, 4},
+		expected:    4,
+	},
+}
+
+type testCaseMap struct {
+	description string
+	list        IntList
+	function    func(int) int
+	functionStr string
+	expected    IntList
+}
+
+var testCasesMap = []testCaseMap{
+	{
+		description: "empty list",
+		list:        IntList{},
+		function:    func(x int) int { return x + 1 },
+		functionStr: "(x) -> x + 1",
+		expected:    IntList{},
+	},
+	{
+		description: "non-empty list",
+		list:        IntList{1, 3, 5, 7},
+		function:    func(x int) int { return x + 1 },
+		functionStr: "(x) -> x + 1",
+		expected:    IntList{2, 4, 6, 8},
+	},
+}
+
+type testCaseReverse struct {
+	description string
+	list        IntList
+	expected    IntList
+}
+
+var testCasesReverse = []testCaseReverse{
+	{
+		description: "empty list",
+		list:        IntList{},
+		expected:    IntList{},
+	},
+	{
+		description: "non-empty list",
+		list:        IntList{1, 3, 5, 7},
+		expected:    IntList{7, 5, 3, 1},
+	},
+}
---
+++ go/list-ops/list_ops_test.go
@@ -0,0 +1,110 @@
+package listops
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+	"strconv"
+	"testing"
+)
+
+func sliceIntToStr(ints []int) string {
+	parts := make([]string, len(ints))
+	for i, v := range ints {
+		parts[i] = strconv.Itoa(v)
+	}
+	return fmt.Sprintf("%s", strings.Join(parts, ", "))
+}
+
+func String(l IntList) string {
+	return fmt.Sprintf("IntList{%s}", sliceIntToStr(l))
+}
+
+func sliceString(l []IntList) string {
+	parts := make([]string, len(l))
+	for i, p := range l {
+		parts[i] = fmt.Sprintf("{%s}", sliceIntToStr(p))
+	}
+	return fmt.Sprintf("[]IntList{%s}", strings.Join(parts, ", "))
+}
+
+func TestFoldl(t *testing.T) {
+	for _, tc := range testCasesFoldl {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := tc.list.Foldl(tc.function, tc.initial); got != tc.expected {
+				t.Fatalf("%s.Foldl(%s, %d) = %d, want: %d", String(tc.list), tc.functionStr, tc.initial, got, tc.expected)
+			}
+		})
+	}
+}
+
+func TestFoldr(t *testing.T) {
+	for _, tc := range testCasesFoldr {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := tc.list.Foldr(tc.function, tc.initial); got != tc.expected {
+				t.Fatalf("%s.Foldr(%s, %d) = %d, want: %d", String(tc.list), tc.functionStr, tc.initial, got, tc.expected)
+			}
+		})
+	}
+}
+
+func TestFilter(t *testing.T) {
+	for _, tc := range testCasesFilter {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := tc.list.Filter(tc.function); !slices.Equal(got, tc.expected) {
+				t.Fatalf("%s.Filter(%s) = %s, want: %s", String(tc.list), tc.functionStr, String(got), String(tc.expected))
+			}
+		})
+	}
+}
+
+func TestLength(t *testing.T) {
+	for _, tc := range testCasesLength {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := tc.list.Length(); got != tc.expected {
+				t.Fatalf("%s.Length() = %d, want: %d", String(tc.list), got, tc.expected)
+			}
+		})
+	}
+}
+
+func TestMap(t *testing.T) {
+	for _, tc := range testCasesMap {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := tc.list.Map(tc.function); !slices.Equal(got, tc.expected) {
+				t.Fatalf("%s.Map(%s) = %s, want: %s", String(tc.list), tc.functionStr, String(got), String(tc.expected))
+			}
+		})
+	}
+}
+
+func TestReverse(t *testing.T) {
+	for _, tc := range testCasesReverse {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := tc.list.Reverse(); !slices.Equal(got, tc.expected) {
+				t.Fatalf("%s.Reverse() = %s, want: %s", String(tc.list), String(got), String(tc.expected))
+			}
+		})
+	}
+}
+
+func TestAppend(t *testing.T) {
+	for _, tc := range testCasesAppend {
+		t.Run(tc.description, func(t *testing.T) {
+			if got := tc.list1.Append(tc.list2); !slices.Equal(got, tc.expected) {
+				t.Fatalf("%s.Append(%s) = %s, want: %s", String(tc.list1), String(tc.list2), String(got), String(tc.expected))
+			}
+		})
+	}
+}
+
+func TestConcat(t *testing.T) {
+	for _, tc := range testCasesConcat {
+		t.Run(tc.description, func(t *testing.T) {
+			var initial IntList
+			if got := initial.Concat(tc.lists); !slices.Equal(got, tc.expected) {
+				t.Fatalf("%s.Concat(%s) = %s, want: %s", String(initial), sliceString(tc.lists), String(got), String(tc.expected))
+			}
+		})
+	}
+}
