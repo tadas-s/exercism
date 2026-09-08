ef9ac31971be9c93dbfcaa4bce1cb1d5d752f579
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-08 11:31:31 +0300

    go: etl exercise

---
+++ go/etl/cases_test.go
@@ -0,0 +1,35 @@
+package etl
+
+// This is an auto-generated file. Do not change it manually. Run the generator to update the file.
+// See https://github.com/exercism/go#synchronizing-tests-and-instructions
+// Source: exercism/problem-specifications
+// Commit: 25c2ae5 build(deps): bump fast-uri from 3.1.0 to 3.1.2 (#2653)
+
+type testCase struct {
+	description string
+	input       map[int][]string
+	expected    map[string]int
+}
+
+var testCases = []testCase{
+	{
+		description: "single letter",
+		input:       map[int][]string{1: []string{"A"}},
+		expected:    map[string]int{"a": 1},
+	},
+	{
+		description: "single score with multiple letters",
+		input:       map[int][]string{1: []string{"A", "E", "I", "O", "U"}},
+		expected:    map[string]int{"a": 1, "e": 1, "i": 1, "o": 1, "u": 1},
+	},
+	{
+		description: "multiple scores with multiple letters",
+		input:       map[int][]string{1: []string{"A", "E"}, 2: []string{"D", "G"}},
+		expected:    map[string]int{"a": 1, "d": 2, "e": 1, "g": 2},
+	},
+	{
+		description: "multiple scores with differing numbers of letters",
+		input:       map[int][]string{1: []string{"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"}, 2: []string{"D", "G"}, 3: []string{"B", "C", "M", "P"}, 4: []string{"F", "H", "V", "W", "Y"}, 5: []string{"K"}, 8: []string{"J", "X"}, 10: []string{"Q", "Z"}},
+		expected:    map[string]int{"a": 1, "b": 3, "c": 3, "d": 2, "e": 1, "f": 4, "g": 2, "h": 4, "i": 1, "j": 8, "k": 5, "l": 1, "m": 3, "n": 1, "o": 1, "p": 3, "q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8, "y": 4, "z": 10},
+	},
+}
---
+++ go/etl/etl_test.go
@@ -0,0 +1,24 @@
+package etl
+
+import (
+	"maps"
+	"testing"
+)
+
+func TestTransform(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			if actual := Transform(tc.input); !maps.Equal(actual, tc.expected) {
+				t.Fatalf("Transform(%v)\ngot: %v\nwant: %v", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkTransform(b *testing.B) {
+	for b.Loop() {
+		for _, tc := range testCases {
+			Transform(tc.input)
+		}
+	}
+}
