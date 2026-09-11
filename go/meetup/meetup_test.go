d12ef878627ed313741a7d8d380bb60879fc81ad
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-11 13:40:10 +0300

    go: meetup exercise

---
+++ go/meetup/meetup_test.go
@@ -0,0 +1,31 @@
+package meetup
+
+import "testing"
+
+var weekName = map[WeekSchedule]string{
+	First:  "first",
+	Second: "second",
+	Third:  "third",
+	Fourth: "fourth",
+	Teenth: "teenth",
+	Last:   "last",
+}
+
+func TestDay(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			actual := Day(tc.week, tc.weekday, tc.month, tc.year)
+			if actual != tc.expectedDay {
+				t.Fatalf("Day(%q, %d, %d, %d) = %d, want: %d", weekName[tc.week], tc.weekday, tc.month, tc.year, actual, tc.expectedDay)
+			}
+		})
+	}
+}
+
+func BenchmarkDay(b *testing.B) {
+	for b.Loop() {
+		for _, tc := range testCases {
+			Day(tc.week, tc.weekday, tc.month, tc.year)
+		}
+	}
+}
