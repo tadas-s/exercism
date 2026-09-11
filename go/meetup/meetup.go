ea034123c34917093f8064436e3a5420ebc34c4c
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-11 14:37:43 +0300

    go: meetup solution

--- go/meetup/meetup.go
+++ go/meetup/meetup.go
@@ -2,8 +2,45 @@ package meetup
 
 import "time"
 
-// Define the WeekSchedule type here.
+type WeekSchedule int
+
+const (
+	First WeekSchedule = iota
+	Second
+	Third
+	Fourth
+	Last
+	Teenth
+)
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
-	panic("Please implement the Day function")
+	nthCounter := 0
+	lastWeekday := -1
+
+	for currentDay := 1; currentDay <= 31; currentDay++ {
+		date := time.Date(year, month, currentDay, 12, 0, 0, 0, time.UTC)
+
+		// If month was bumped - we're looping month with less than 31 days
+		if date.Month() != month {
+			break
+		}
+
+		if date.Weekday() != wDay {
+			continue
+		}
+
+		switch {
+		case wSched == Teenth && date.Day() >= 13 && date.Day() <= 19:
+			return currentDay
+		case wSched >= First && wSched <= Fourth:
+			if nthCounter == int(wSched) {
+				return currentDay
+			}
+			nthCounter++
+		case wSched == Last:
+			lastWeekday = currentDay
+		}
+	}
+
+	return lastWeekday // for when wSched == Last
 }
