be5c9c34ad9a7eb1f24233aea4df812100ebfd4d
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-11 13:37:41 +0300

    go: word-count solution

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,7 +1,30 @@
 package wordcount
 
+import (
+	"strings"
+	"unicode"
+)
+
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+	frequencies := Frequency{}
+
+	split := strings.FieldsFunc(phrase, func(c rune) bool {
+		return (unicode.IsSpace(c) || unicode.IsPunct(c)) && !(c == '\'')
+	})
+
+	for _, possiblyWord := range split {
+		possiblyWord = strings.Trim(strings.ToLower(possiblyWord), "'")
+
+		if len(possiblyWord) == 1 && !unicode.IsLetter(rune(possiblyWord[0])) && !unicode.IsDigit(rune(possiblyWord[0])) {
+			continue
+		}
+
+		if len(possiblyWord) > 0 {
+			frequencies[possiblyWord]++
+		}
+	}
+
+	return frequencies
 }
