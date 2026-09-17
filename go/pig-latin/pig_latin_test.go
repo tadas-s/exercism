621ce13815385a358c3afc40f9f7d03f12c93019
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-17 15:31:39 +0300

    go: pig-latin exercise

---
+++ go/pig-latin/pig_latin_test.go
@@ -0,0 +1,23 @@
+package piglatin
+
+import (
+	"testing"
+)
+
+func TestPigLatin(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			if actual := Sentence(tc.input); actual != tc.expected {
+				t.Fatalf("Sentence(%q) = %q, want %q", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkSentence(b *testing.B) {
+	for b.Loop() {
+		for _, tc := range testCases {
+			Sentence(tc.input)
+		}
+	}
+}
