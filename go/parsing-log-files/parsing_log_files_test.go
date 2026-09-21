07f7e9d99429c1de96da3ff60ba0d2151747cafc
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-21 09:44:55 +0300

    go: parsing-log-files exercise

---
+++ go/parsing-log-files/parsing_log_files_test.go
@@ -0,0 +1,199 @@
+package parsinglogfiles
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestIsValidLine(t *testing.T) {
+	tests := []struct {
+		description string
+		text        string
+		expected    bool
+	}{
+		{
+			description: "Valid ERR message",
+			text:        "[ERR] A good error here",
+			expected:    true,
+		},
+		{
+			description: "Valid INF message",
+			text:        "[INF] The latest information",
+			expected:    true,
+		},
+		{
+			description: "Invalid ERR message",
+			text:        "Any old [ERR] text",
+			expected:    false,
+		},
+		{
+			description: "Invalid INF message",
+			text:        "bad start to [INF] Message",
+			expected:    false,
+		},
+		{
+			description: "Invalid tag",
+			text:        "[BOB] Any old text",
+			expected:    false,
+		},
+		{
+			description: "Line with less characters than 5",
+			text:        "text",
+			expected:    false,
+		},
+		{
+			description: "Empty line",
+			text:        "",
+			expected:    false,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			got := IsValidLine(tc.text)
+			want := tc.expected
+			if want != got {
+				t.Fatalf("expected: %v, got: %v", want, got)
+			}
+		})
+	}
+}
+
+func TestSplitLogLine(t *testing.T) {
+	tests := []struct {
+		description string
+		text        string
+		expected    []string
+	}{
+		{
+			description: "three sections",
+			text:        "section 1<*>section 2<~~~>section 3",
+			expected:    []string{"section 1", "section 2", "section 3"},
+		},
+		{
+			description: "three sections with different symbols inside angular brackets",
+			text:        "section 1<=>section 2<-*~*->section 3",
+			expected:    []string{"section 1", "section 2", "section 3"},
+		},
+		{
+			description: "two sections with no characters between angular brackets",
+			text:        "section 1<>section 2",
+			expected:    []string{"section 1", "section 2"},
+		},
+		{
+			description: "single section with some angular brackets",
+			text:        "<start> <end>",
+			expected:    []string{"<start> <end>"},
+		},
+		{
+			description: "empty text",
+			text:        "",
+			expected:    []string{""},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			got := SplitLogLine(tc.text)
+			want := tc.expected
+			if !slices.Equal(want, got) {
+				t.Fatalf("expected: %v, got: %v", want, got)
+			}
+		})
+	}
+}
+
+func TestCountQuotedPasswords(t *testing.T) {
+	tests := []struct {
+		description string
+		lines       []string
+		expected    int
+	}{
+		{
+			description: "text with two matches",
+			lines: []string{
+				``,
+				`[INF] passWord`,
+				`"passWord"`,
+				`[INF] User saw error message "Unexpected Error" on page load.`,
+				`[INF] The message "Please reset your password" was ignored by the user`,
+			},
+			expected: 2,
+		},
+		{
+			description: "text with no matches",
+			lines: []string{
+				`passWord"passWor"`,
+			},
+			expected: 0,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			got := CountQuotedPasswords(tc.lines)
+			want := tc.expected
+			if want != got {
+				t.Fatalf("expected: %v, got: %v", want, got)
+			}
+		})
+	}
+}
+
+func TestRemoveEndOfLineText(t *testing.T) {
+	tests := []struct {
+		description string
+		text        string
+		expected    string
+	}{
+		{
+			description: "INF message",
+			text:        "[INF] end-of-line23033 Network Failure end-of-line27",
+			expected:    "[INF]  Network Failure ",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			got := RemoveEndOfLineText(tc.text)
+			want := tc.expected
+			if want != got {
+				t.Fatalf("expected: %v, got: %v", want, got)
+			}
+		})
+	}
+}
+
+func TestTagWithUserName(t *testing.T) {
+	tests := []struct {
+		description string
+		lines       []string
+		expected    []string
+	}{
+		{
+			description: "INF message",
+			lines: []string{
+				"[WRN] User James123 has exceeded storage space.",
+				"[WRN] Host down. User   Michelle4 lost connection.",
+				"[INF] Users can login again after 23:00.",
+				"[DBG] We need to check that user names are at least 6 chars long.",
+			},
+			expected: []string{
+				"[USR] James123 [WRN] User James123 has exceeded storage space.",
+				"[USR] Michelle4 [WRN] Host down. User   Michelle4 lost connection.",
+				"[INF] Users can login again after 23:00.",
+				"[DBG] We need to check that user names are at least 6 chars long.",
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			got := TagWithUserName(tc.lines)
+			want := tc.expected
+			if !slices.Equal(want, got) {
+				for i := range got {
+					if want[i] != got[i] {
+						t.Fatalf("expected: %v, got: %v", want[i], got[i])
+					}
+				}
+				t.Fatalf("expected: %v, got: %v", want, got)
+			}
+		})
+	}
+}
