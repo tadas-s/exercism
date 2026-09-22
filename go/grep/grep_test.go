c69c8b9467fe9c731ce6588226222c24da90087e
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-22 10:53:46 +0300

    go: grep exercise

---
+++ go/grep/grep_test.go
@@ -0,0 +1,90 @@
+package grep
+
+import (
+	"os"
+	"slices"
+	"strings"
+	"testing"
+)
+
+func createFiles(content []string) (filenames []string) {
+	// Parse fileContentData, making the list of filenames
+	// with their content.
+	var filename string
+	var f *os.File
+	for _, d := range content {
+		t := strings.TrimSpace(d)
+		if t == "" {
+			if filename == "" {
+				continue
+			}
+			// Close file
+			f.Close()
+			filenames = append(filenames, filename)
+			filename = ""
+			f = nil
+			continue
+		}
+		if strings.Contains(t, ".txt") {
+			filename = t
+			// Open file
+			var err error
+			f, err = os.Create(filename)
+			if err != nil {
+				panic(err)
+			}
+			continue
+		}
+		fields := strings.Split(t, "|")
+		if len(fields) == 3 {
+			// Write string into file with newline.
+			_, err := f.WriteString(strings.TrimRight(fields[1], " ") + "\n")
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+	if f != nil {
+		f.Close()
+		filenames = append(filenames, filename)
+	}
+	return filenames
+}
+
+func deleteFiles(filenames []string) {
+	for _, file := range filenames {
+		os.Remove(file)
+	}
+}
+
+func TestSearch(t *testing.T) {
+	files := createFiles(fileContentData)
+	defer deleteFiles(files)
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			actual := Search(tc.pattern, tc.flags, tc.files)
+
+			// We do not care whether the result is nil or an empty slice.
+			if len(tc.expected) == 0 && len(actual) == 0 {
+				return
+			}
+
+			if !slices.Equal(actual, tc.expected) {
+				t.Errorf("Search(%q, %q, %q)\ngot: %q\nwant: %q", tc.pattern, tc.flags, tc.files, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkSearch(b *testing.B) {
+	files := createFiles(fileContentData)
+	defer deleteFiles(files)
+
+	b.ResetTimer()
+	for b.Loop() {
+		for _, tc := range testCases {
+			Search(tc.pattern, tc.flags, tc.files)
+		}
+	}
+}
