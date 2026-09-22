ff4a309808099c25824c9f60af997077579f0548
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-22 15:09:02 +0300

    go: pangram solution

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,15 @@
 package pangram
 
+import "strings"
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	lowercased := strings.ToLower(input)
+
+	for c := 'a'; c <= 'z'; c++ {
+		if !strings.ContainsRune(lowercased, c) {
+			return false
+		}
+	}
+
+	return true
 }
