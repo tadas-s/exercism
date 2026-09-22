97646994beeaa1d2001aa52cc53c97c7f7dbc34c
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-22 15:39:05 +0300

    go: allergies solution

--- go/allergies/allergies.go
+++ go/allergies/allergies.go
@@ -1,9 +1,32 @@
 package allergies
 
+var allergens = map[string]uint{
+	"eggs":         1,
+	"peanuts":      2,
+	"shellfish":    4,
+	"strawberries": 8,
+	"tomatoes":     16,
+	"chocolate":    32,
+	"pollen":       64,
+	"cats":         128,
+}
+
 func Allergies(allergies uint) []string {
-	panic("Please implement the Allergies function")
+	var result []string
+
+	for allergen := range allergens {
+		if AllergicTo(allergies, allergen) {
+			result = append(result, allergen)
+		}
+	}
+
+	return result
 }
 
 func AllergicTo(allergies uint, allergen string) bool {
-	panic("Please implement the AllergicTo function")
+	if bitmask, ok := allergens[allergen]; ok {
+		return (allergies & bitmask) != 0
+	}
+
+	return false
 }
