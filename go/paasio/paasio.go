f74fa94d5c89a12b692a232dba1b5e5fa7bb86be
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-23 15:45:50 +0300

    go: paasio solution

--- go/paasio/paasio.go
+++ go/paasio/paasio.go
@@ -1,35 +1,82 @@
 package paasio
 
-import "io"
+import (
+	"io"
+	"sync"
+)
 
-// Define readCounter and writeCounter types here.
+type writeCounter struct {
+	writer io.Writer
 
-// For the return of the function NewReadWriteCounter, you must also define a type that satisfies the ReadWriteCounter interface.
+	writes   int64
+	writeOps int
+
+	mutex *sync.RWMutex
+}
+
+type readCounter struct {
+	reader io.Reader
+
+	reads   int64
+	readOps int
+
+	mutex *sync.RWMutex
+}
+
+type readWriteCounter struct {
+	ReadCounter
+	WriteCounter
+}
 
 func NewWriteCounter(writer io.Writer) WriteCounter {
-	panic("Please implement the NewWriterCounter function")
+	return &writeCounter{writer, 0, 0, new(sync.RWMutex)}
 }
 
 func NewReadCounter(reader io.Reader) ReadCounter {
-	panic("Please implement the NewReadCounter function")
+	return &readCounter{reader, 0, 0, new(sync.RWMutex)}
 }
 
 func NewReadWriteCounter(readwriter io.ReadWriter) ReadWriteCounter {
-	panic("Please implement the NewReadWriteCounter function")
+	return &readWriteCounter{
+		NewReadCounter(readwriter),
+		NewWriteCounter(readwriter),
+	}
 }
 
 func (rc *readCounter) Read(p []byte) (int, error) {
-	panic("Please implement the Read function")
+	rc.mutex.Lock()
+	defer rc.mutex.Unlock()
+
+	read, err := rc.reader.Read(p)
+
+	rc.readOps += 1
+	rc.reads += int64(read)
+
+	return read, err
 }
 
 func (rc *readCounter) ReadCount() (int64, int) {
-	panic("Please implement the ReadCount function")
+	rc.mutex.Lock()
+	defer rc.mutex.Unlock()
+
+	return rc.reads, rc.readOps
 }
 
 func (wc *writeCounter) Write(p []byte) (int, error) {
-	panic("Please implement the Write function")
+	wc.mutex.Lock()
+	defer wc.mutex.Unlock()
+
+	written, err := wc.writer.Write(p)
+
+	wc.writeOps += 1
+	wc.writes += int64(written)
+
+	return written, err
 }
 
 func (wc *writeCounter) WriteCount() (int64, int) {
-	panic("Please implement the WriteCount function")
+	wc.mutex.Lock()
+	defer wc.mutex.Unlock()
+
+	return wc.writes, wc.writeOps
 }
