a9c2c7ea407e22f1110d10c9e564a82104293e58
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-23 13:53:17 +0300

    go: high-scores exercise

---
+++ go/high-scores/high_scores_test.go
@@ -0,0 +1,39 @@
+package highscores
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestHighScoreIntFuncs(t *testing.T) {
+	for _, tc := range intTestCases {
+		t.Run(tc.description, func(t *testing.T) {
+			h := NewHighScores(tc.input)
+			if actual := tc.call(h); actual != tc.expected {
+				t.Fatalf("h = NewHighScores(%#v); %s = %d, want: %d", tc.input, tc.callMsg, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func TestHighScoreSliceFuncs(t *testing.T) {
+	for _, tc := range sliceTestCases {
+		t.Run(tc.description, func(t *testing.T) {
+			h := NewHighScores(tc.input)
+			if actual := tc.call(h); !slices.Equal(actual, tc.expected) {
+				t.Fatalf("h = NewHighScores(%#v); %s = %#v, want: %#v", tc.input, tc.callMsg, actual, tc.expected)
+			}
+		})
+	}
+}
+
+func BenchmarkHighScores(b *testing.B) {
+	for b.Loop() {
+		for _, tc := range intTestCases {
+			tc.call(NewHighScores(tc.input))
+		}
+		for _, tc := range sliceTestCases {
+			tc.call(NewHighScores(tc.input))
+		}
+	}
+}
