12c183e5105ff69ce07361c3f846f2457009b976
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-25 12:49:21 +0300

    go: reverse-string solution

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,5 +1,11 @@
 package reversestring
 
 func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+	out := []rune(input)
+
+	for i := 0; i < len(out)/2; i++ {
+		out[i], out[len(out)-1-i] = out[len(out)-1-i], out[i]
+	}
+
+	return string(out)
 }
