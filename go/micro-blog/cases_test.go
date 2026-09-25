d02a55c786465c7f0e446b5df5d684bc9e9e2e97
Tadas Sasnauskas <tadas@yoyo.lt>
2026-09-25 13:12:48 +0300

    go: micro-blog exercise

---
+++ go/micro-blog/cases_test.go
@@ -0,0 +1,75 @@
+package microblog
+
+// This is an auto-generated file. Do not change it manually. Run the generator to update the file.
+// See https://github.com/exercism/go#synchronizing-tests-and-instructions
+// Source: exercism/problem-specifications
+// Commit: 25c2ae5 build(deps): bump fast-uri from 3.1.0 to 3.1.2 (#2653)
+
+type testCase struct {
+	description string
+	input       string
+	expected    string
+}
+
+var testCases = []testCase{
+	{
+		description: "English language short",
+		input:       "Hi",
+		expected:    "Hi",
+	},
+	{
+		description: "English language long",
+		input:       "Hello there",
+		expected:    "Hello",
+	},
+	{
+		description: "German language short (broth)",
+		input:       "brühe",
+		expected:    "brühe",
+	},
+	{
+		description: "German language long (bear carpet → beards)",
+		input:       "Bärteppich",
+		expected:    "Bärte",
+	},
+	{
+		description: "Bulgarian language short (good)",
+		input:       "Добър",
+		expected:    "Добър",
+	},
+	{
+		description: "Greek language short (health)",
+		input:       "υγειά",
+		expected:    "υγειά",
+	},
+	{
+		description: "Maths short",
+		input:       "a=πr²",
+		expected:    "a=πr²",
+	},
+	{
+		description: "Maths long",
+		input:       "∅⊊ℕ⊊ℤ⊊ℚ⊊ℝ⊊ℂ",
+		expected:    "∅⊊ℕ⊊ℤ",
+	},
+	{
+		description: "English and emoji short",
+		input:       "Fly 🛫",
+		expected:    "Fly 🛫",
+	},
+	{
+		description: "Emoji short",
+		input:       "💇",
+		expected:    "💇",
+	},
+	{
+		description: "Emoji long",
+		input:       "❄🌡🤧🤒🏥🕰😀",
+		expected:    "❄🌡🤧🤒🏥",
+	},
+	{
+		description: "Royal Flush?",
+		input:       "🃎🂸🃅🃋🃍🃁🃊",
+		expected:    "🃎🂸🃅🃋🃍",
+	},
+}
